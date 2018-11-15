8411dd777f895d08f8ee7385a4aa9ce8f2637966
Sylvain <sylvain@lazybird.co>
2018-11-15 17:40:08 +0100

    Simplify entrypoint and env variables

--- docker/django/docker-entrypoint.sh
+++ docker/django/docker-entrypoint.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
 set -e
-
-case "$1" in
-    setup)
-        echo "Setting up project"
-        python manage.py collectstatic --noinput
-        python manage.py migrate
-    ;;
-    dev)
-        echo "Running Development Server on 0.0.0.0:${PORT}"
-        python manage.py runserver 0.0.0.0:${PORT}
-    ;;
-    bash)
-        /bin/bash "${@:2}"
-    ;;
-    manage)
-        python manage.py "${@:2}"
-    ;;
-    python)
-        python "${@:2}"
-    ;;
-    shell)
-        python manage.py shell_plus
-    ;;
-    uwsgi)
-        echo "Running uWSGI..."
-        uwsgi --ini /uwsgi.ini
-    ;;
-    *)
-        echo "Running Command $@"
-        exec "$@"
-    ;;
-esac
+envsubst < /uwsgi.ini.template > /uwsgi.ini
+echo "Setting up project"
+python manage.py collectstatic --noinput
+python manage.py migrate
+echo "Running Command $@"
+exec "$@"
