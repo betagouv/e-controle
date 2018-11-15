6a8707b837f819e532702f7b5dc52ecadcfea036
Sylvain <sylvain@lazybird.co>
2018-11-15 09:54:37 +0100

    Remove sample env files - convention over configuration

--- ecc/ecc/settings.py
+++ ecc/ecc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('POSTGRES_DB_NAME')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', False))
