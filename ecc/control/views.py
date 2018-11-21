db2c06b655b71ddd31b94cdfde0bf66fa54a4da1
Sylvain <sylvain@lazybird.co>
2018-11-21 11:27:38 +0100

    Integrate questionnaire list data in templates

--- ecc/control/views.py
+++ ecc/control/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+from .models import Questionnaire
+
+
+class QuestionnaireList(ListView):
+    template_name = "ecc/questionnaire_list.html"
+    context_object_name = 'questionnaires'
+
+    def get_queryset(self):
+        return Questionnaire.objects.filter(control=self.kwargs.get('control_id'))
+
+
+questionnaire_list = QuestionnaireList.as_view()
--- ecc/ecc/urls.py
+++ ecc/ecc/urls.py
@@ -4,9 +4,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from . import views as ecc_views
+from control import views as control_views
 
 urlpatterns = [
     path('', ecc_views.login, name='login'),
+    path('controle/<int:control_id>/', control_views.questionnaire_list),
     path('accueil/', ecc_views.questionnaire_list, name='questionnaire-list'),
     path('questionnaire/', ecc_views.questionnaire_detail, name='questionnaire-detail'),
     path('question/', ecc_views.question_detail, name='question-detail'),
