417a66d29f39de36dcb424699a35f06f3c0a5f1b
Sylvain <sylvain@lazybird.co>
2018-12-10 15:12:55 +0100

    Add support for sending token email

--- ecc/magicauth/forms.py
+++ ecc/magicauth/forms.py
@@ -1,7 +1,11 @@
 from django import forms
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.template import loader
 from django.utils.translation import ugettext_lazy as _
 
+from .models import MagicToken
 
 User = get_user_model()
 
@@ -15,6 +19,29 @@ class EmailForm(forms.Form):
             raise forms.ValidationError(_(f"Aucun utilisateur trouvé"))
         return user_email
 
+    def create_token(self, user):
+        token = MagicToken.objects.create(user=user)
+        return token
+
     def send_email(self):
-        print('envoie ok')
-        pass
+        user_email = self.cleaned_data['email']
+        user = User.objects.get(email=user_email)
+        token = self.create_token(user)
+        email_subject = getattr(settings, 'MAGICAUTH_EMAIL_SUBJECT', 'Connection e-controle')
+        html_template = getattr(settings, 'MAGICAUTH_EMAIL_HTML_TEMPLATE', 'magicauth/email.html')
+        text_template = getattr(settings, 'MAGICAUTH_EMAIL_TEXT_TEMPLATE', 'magicauth/email.txt')
+        from_email = getattr(settings, 'MAGICAUTH_FROM_EMAIL', 'econtrole@bate.ccomptes.fr')
+        context = {
+            'token': token,
+            'user': user,
+        }
+        text_message = loader.render_to_string(text_template, context)
+        html_message = loader.render_to_string(html_template, context)
+        send_mail(
+            subject=email_subject,
+            message=text_message,
+            from_email=from_email,
+            html_message=html_message,
+            recipient_list=[user_email],
+            fail_silently=False,
+        )
