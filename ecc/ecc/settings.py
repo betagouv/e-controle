7b1a12387ad3a700a386fd160bf1b083993e6a63
Sylvain <sylvain@lazybird.co>
2018-12-10 09:37:06 +0100

    Add emai sening settings

--- ecc/ecc/settings.py
+++ ecc/ecc/settings.py
@@ -100,6 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_HOST = env('EMAIL_HOST', default='debugmail.io')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT', default=25)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
