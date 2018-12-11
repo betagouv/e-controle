07413565822a97503ef6a660f08f8094ea6ecdd0
Sylvain <sylvain@lazybird.co>
2018-12-11 05:41:37 +0100

    Add suport for loading env file

--- ecc/ecc/wsgi.py
+++ ecc/ecc/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecc.settings")
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path, override=True)
 
 application = get_wsgi_application()
