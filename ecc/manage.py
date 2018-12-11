2aedcb91aa19268bc828a4262328106871cf2cd3
Sylvain <sylvain@lazybird.co>
2018-12-11 14:16:59 +0100

    Move environnement vars in a separate file

--- ecc/manage.py
+++ ecc/manage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+from dotenv import load_dotenv
+from pathlib import Path
 import os
 import sys
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecc.settings")
@@ -19,4 +22,6 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path, override=True)
     execute_from_command_line(sys.argv)
