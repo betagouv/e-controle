07b59a9db6de2048052a34521d16914ad610575f
Sylvain <sylvain@lazybird.co>
2018-12-12 17:13:29 +0100

    Add ability to set MEDIA ROOT from environnment vars

--- ecc/ecc/settings.py
+++ ecc/ecc/settings.py
@@ -1,13 +1,12 @@
 import os
 import environ
+from pathlib import Path
 
 env = environ.Env(
     DEBUG=(bool, False),
 )
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path('.').absolute()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -138,4 +137,5 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = env('MEDIA_ROOT', default=DEFAULT_MEDIA_ROOT)
