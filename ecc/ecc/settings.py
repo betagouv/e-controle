d0ba339959b892a4e6fb5f41874cc420f01d5be1
Sylvain <sylvain@lazybird.co>
2018-12-13 12:22:04 +0100

    Add email ssl/tls support

--- ecc/ecc/settings.py
+++ ecc/ecc/settings.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 env = environ.Env(
     DEBUG=(bool, False),
+    EMAIL_USE_TLS=(bool, False),
+    EMAIL_USE_SSL=(bool, False),
 )
 
 BASE_DIR = Path('.').absolute()
@@ -103,11 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER',)
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER',)
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+EMAIL_USE_SSL = env('EMAIL_USE_SSL')
 
 LOGIN_REDIRECT_URL = 'questionnaire-list'
 
