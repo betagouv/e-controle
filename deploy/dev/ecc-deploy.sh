f0c3c7a2365c709a06ba3126e6efa748aa5b60be
Sylvain <sylvain@lazybird.co>
2018-12-13 12:28:54 +0100

    Add deployment script

---
+++ deploy/dev/ecc-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PROJECT_DIR=/opt/e-controle/
+CODE_DIR=$PROJECT_DIR/ecc/
+VENV_DIR=/opt/venv/
+CONF_DIR=/opt/conf
+
+# Set environnement
+source $PROJECT_DIR/.env
+
+# We need a proxy to acces the web
+export {http,https,ftp}_proxy=$PROXY
+
+# Django deployment
+
+source $VENV_DIR/bin/activate
+
+cd $CODE_DIR
+git pull
+pip install -r requirements.txt
+./manage.py migrate
+./manage.py collectstatic --noinput
+
+# Run uWSGI
+uwsgi --ini $CONF_DIR/uwsgi.ini
