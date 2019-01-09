dce5cba45e4bfc3d0010488c5f23dc224473123c
Sylvain <sylvain@lazybird.co>
2019-01-09 11:44:51 +0100

    Add questionnaire folder to upload path

--- control/upload_path.py
+++ control/upload_path.py
@@ -5,6 +5,7 @@ def response_file_path(instance, filename):
     theme_num = instance.question.theme.numbering
     theme_folder = f'THEME-{theme_num}'
     questionaire_num = instance.question.theme.questionnaire.numbering
+    questionnaire_folder = f'QUESTIONNAIRE-{questionaire_num:02}'
     question_num = instance.question.numbering
     prefixed_filename = f'Q{questionaire_num:02}-{theme_num:02}-{question_num:02}-{filename}'
-    return f'{control_folder}/{theme_folder}/{prefixed_filename}'
+    return f'{control_folder}/{questionnaire_folder}/{theme_folder}/{prefixed_filename}'
