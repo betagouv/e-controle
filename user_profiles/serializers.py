1343d8917f42102a7935079dd08c811cadfd6214
Sylvain <sylvain@lazybird.co>
2019-04-09 01:36:54 +0200

    Add endpoint for creating user

--- user_profiles/serializers.py
+++ user_profiles/serializers.py
@@ -8,6 +8,14 @@ from .models import UserProfile
 User = get_user_model()
 
 
+class ControlPKField(serializers.PrimaryKeyRelatedField):
+
+    def get_queryset(self):
+        user = self.context['request'].user
+        queryset = user.profile.controls.all()
+        return queryset
+
+
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -17,7 +25,22 @@ class UserSerializer(serializers.ModelSerializer):
 
 class UserProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    controls = ControlPKField(many=True)
 
     class Meta:
         model = UserProfile
-        fields = ('user', 'controls', 'profile_type')
+        fields = ('user', 'profile_type', 'organization', 'controls')
+        extra_kwargs = {'controls': {'write_only': True}}
+
+    def create(self, validated_data):
+        profile_data = validated_data
+        controls_data = profile_data.pop('controls')
+        user_data = profile_data.pop('user')
+        user_data['username'] = user_data['email']
+        user = User.objects.create(**user_data)
+        profile_data['user'] = user
+        profile_data['send_files_report'] = True
+        profile = UserProfile.objects.create(**profile_data)
+        for control_data in controls_data:
+            profile.controls.add(control_data)
+        return profile
