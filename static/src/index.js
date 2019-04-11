a04cd80f12c2bd2c51195e7ecab2d18ee5bd6fd4
Sylvain <sylvain@lazybird.co>
2019-04-11 17:58:30 +0200

    Change vue managed app html id

--- static/src/index.js
+++ static/src/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue/dist/vue.js'
 import User from './users/User.vue'
 
-var useHandler = new Vue({
-  el: '#questionnaire-list-app',
+var vm1 = new Vue({
+  el: '#vm-app-1',
   data: {
   },
   components: {
