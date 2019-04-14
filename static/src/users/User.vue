455aeafad62930007ddc8c59b1b65df6fa82d466
Sylvain <sylvain@lazybird.co>
2019-04-14 22:05:22 +0200

    Add user list managed by vuejs

--- static/src/users/User.vue
+++ static/src/users/User.vue
@@ -1,17 +1,61 @@
 <template>
-<div>
-  Demo User App {{ users }}
+<div class="card card-collapsed border-0 mb-0">
+  <div class="card-header border-0 pl-0" data-toggle="card-collapse">
+    <h5 class="card-title font-weight-bold"><a href="#"><i class="dropdown-icon fe fe-users"></i>Personnes ayant accès au contrôle</a></h5>
+  </div>
+  <div class="card-body">
+
+    <user-list :users=inspectorUsers() profile-type="inspector"></user-list>
+
+    <user-list :users=auditedUsers() profile-type="audited"></user-list>
+
+    <div data-toggle="card-collapse" class="text-center bg-blue cursor-pointer text-white" style="cursor: pointer;"><i class="fe fe-chevron-up"></i></div>
+  </div>
 </div>
 </template>
 
 <script lang="ts">
   import Vue from "vue";
+  import axios from "axios"
+  import VueAxios from "vue-axios"
+
+  import UserList from "./UserList.vue"
+
+  Vue.use(VueAxios, axios)
 
   export default Vue.extend({
+    props: {
+      controlId: Number
+    },
     data() {
       return {
-        users: [1, 2]
+        users: []
       }
+    },
+    methods: {
+      getUsers() {
+        var urlParams = new URLSearchParams()
+        urlParams.set("controls", this.controlId)
+        Vue.axios.get('/api/user/?' + urlParams.toString()).then((response) => {
+          this.users = response.data
+        })
+      },
+      auditedUsers() {
+        return this.users.filter(item => {
+           return item.profile_type == 'audited'
+        })
+      },
+      inspectorUsers() {
+        return this.users.filter(item => {
+           return item.profile_type == 'inspector'
+        })
+      }
+    },
+    mounted() {
+      this.getUsers()
+    },
+    components: {
+      UserList,
     }
   });
 </script>
---
+++ static/src/users/UserList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card" v-if="users && users.length">
+    <div class="card-header">
+      <h3 v-if="profileType=='inspector'" class="card-title"><i class="fa fa-institution mr-2"></i><strong>Équipe de contrôle</strong></h3>
+      <h3 v-if="profileType=='audited'" class="card-title"><i class="fe fe-user mr-2"></i><strong>Organisme controlé</strong></h3>
+    </div>
+    <div class="card-body pr-0">
+      <ul class="list-unstyled list-separated">
+        <li class="list-separated-item" v-for="(user, index) in users" :key="index">
+          <div class="row align-items-center">
+            <div class="col-auto">
+              <span class="avatar avatar-pink">{{ user.first_name.charAt(0) }}{{ user.last_name.charAt(0) }}</span>
+            </div>
+            <div class="col">
+              <a href="javascript:void(0)" class="text-inherit" :title="user.organization">{{ user.first_name }} {{ user.last_name }}</a>
+              <small class="d-block item-except text-sm text-muted h-1x"><a :href="'mailto:' + user.email">{{ user.email }}</a></small>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend({
+    props: {
+      users: Array,
+      profileType: String
+    }
+  });
+</script>
+
+<style></style>
