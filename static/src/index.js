d88bd19bad4d3024aabd6e991382629ebb292168
Sylvain <sylvain@lazybird.co>
2019-04-15 15:38:43 +0200

    Remove unused import statements

--- static/src/index.js
+++ static/src/index.js
@@ -1,9 +1,6 @@
 import Users from './users/Users.vue'
 import Vue from 'vue/dist/vue.js'
 
-const EventBus = new Vue();
-export default EventBus;
-
 var vm1 = new Vue({
   el: '#questionnaire-list-vm',
   data: {
