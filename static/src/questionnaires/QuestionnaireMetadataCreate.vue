07a97d6b8bec6f56d0cb48c698a58a5130a57b8f
Estelle Comment <estelle.comment@beta.gouv.fr>
2019-04-15 18:10:49 +0200

    Rename formdata to metadata

--- static/src/questionnaires/QuestionnaireMetadataCreate.vue
+++ static/src/questionnaires/QuestionnaireMetadataCreate.vue
@@ -5,12 +5,12 @@
       <fieldset class="form-fieldset">
         <div class="form-group">
           <label class="form-label">Date de transmission du questionnaire<span class="form-required"></span></label>
-          <input type="text" class="form-control" v-bind:class="{ 'state-invalid': errors.sent_date }" v-model="formData.sent_date">
+          <input type="text" class="form-control" v-bind:class="{ 'state-invalid': errors.sent_date }" v-model="metadata.sent_date">
           <p class="text-muted pl-2" v-if="errors.sent_date"><i class="fa fa-warning"></i> {{ errors.sent_date.join(' / ')}}</p>
         </div>
         <div class="form-group">
           <label class="form-label">Date de réponse souhaitée<span class="form-required"></span></label>
-          <input type="text" class="form-control" v-bind:class="{ 'state-invalid': errors.end_date }" v-model="formData.end_date">
+          <input type="text" class="form-control" v-bind:class="{ 'state-invalid': errors.end_date }" v-model="metadata.end_date">
           <p class="text-muted pl-2" v-if="errors.end_date"><i class="fa fa-warning"></i> {{ errors.end_date.join(' / ')}}</p>
         </div>
       </fieldset>
@@ -28,7 +28,7 @@
   export default Vue.extend({
     data() {
       return {
-        formData: {
+        metadata: {
             'sent_date': '',
             'end_date': '',
         },
@@ -38,8 +38,8 @@
     methods: {
       createMetadata: function () {
         console.log('metadata created sortof')
-        console.log(this.formData)
-        this.$emit('metadata-created', this.formData)
+        console.log(this.metadata)
+        this.$emit('metadata-created', this.metadata)
       }
     }
   });
