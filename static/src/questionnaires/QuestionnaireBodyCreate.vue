ac7f3ca1917b71ff91796156c055587e251e969c
Estelle Comment <estelle.comment@beta.gouv.fr>
2019-04-16 11:22:11 +0200

    Better css for inputting body

--- static/src/questionnaires/QuestionnaireBodyCreate.vue
+++ static/src/questionnaires/QuestionnaireBodyCreate.vue
@@ -1,28 +1,53 @@
 <template>
   <div>
-
     <form @submit.prevent="createBody">
-      <fieldset v-for="(group, groupIndex) in body" class="form-fieldset" id="'group-' + groupIndex">
-        <div class="form-group theme">
-          <label class="form-label">Thème<span class="form-required"></span></label>
-          <input type="text" class="form-control" v-model="body[groupIndex].theme">
+
+      <div class="card" v-for="(group, groupIndex) in body">
+        <div class="card-status card-status-top bg-blue">
         </div>
-        <div class="form-group questions">
-          <div v-for="(question, qIndex) in body[groupIndex].questions">
-            <label class="form-label">Question {{qIndex + 1}}<span class="form-required"></span></label>
-            <input type="text" class="form-control" v-model="body[groupIndex].questions[qIndex]">
+
+        <div class="card-header">
+          <label v-bind:for="'theme' + (groupIndex + 1)">
+            <h3 class="card-title">{{groupIndex + 1}}.</h3>
+          </label>
+          <input class="form-control"
+                 placeholder="Ecrivez un thème ici"
+                 type="text"
+                 v-bind:id="'theme' + (groupIndex + 1)"
+                 v-model="body[groupIndex].theme">
+        </div>
+
+        <div v-for="(question, qIndex) in body[groupIndex].questions"
+             class="card card-collapsed  border-0 m-0 p-0 pb-0 pt-2 {% cycle '' 'zebra' %}">
+          <div class="card-header border-1" data-toggle="card-collapse" >
+            <label v-bind:for="'question' + (groupIndex + 1) + '.' + (qIndex + 1)">
+              <span class="stamp stamp-md bg-blue mr-3" style="cursor: pointer">
+                {{ groupIndex + 1 }}.{{ qIndex + 1 }}
+              </span>
+            </label>
+            <textarea class="form-control"
+                      placeholder="Ecrivez une question ici"
+                      rows="4"
+                      v-bind:id="'question' + (groupIndex + 1) + '.' + (qIndex + 1)"
+                      v-model="body[groupIndex].questions[qIndex]">
+            </textarea>
           </div>
         </div>
+
         <div @click="addQuestion(groupIndex)">
           +
         </div>
-      </fieldset>
+
+      </div>
+
       <div @click="addGroup()">
         +
       </div>
+
       <div>
         <button type="submit" class="btn btn-primary">Suivant</button>
       </div>
+
     </form>
 
   </div>
