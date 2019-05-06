88b2bef5115212a4daed798111274c432c94aa74
Sylvain <sylvain@lazybird.co>
2019-05-06 14:29:37 +0200

    Remove to close button on add modal

--- static/src/users/AddUserModal.vue
+++ static/src/users/AddUserModal.vue
@@ -5,9 +5,6 @@
     <div class="modal-content">
       <div class="modal-header">
         <h4 class="modal-title" id="labelForModalAddUser">{{ editingControl.title }}</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true"></span>
-        </button>
       </div>
       <div class="modal-body">
         <div v-if="hasErrors" class="alert alert-danger">
