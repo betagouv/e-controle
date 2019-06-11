37dca76af4bef2b8bfe8c06bca4b3ec491a76f69
Sylvain <sylvain@lazybird.co>
2019-06-11 11:50:25 +0200

    Change the way we set test settings

--- ecc/tests/test_pages.py
+++ ecc/tests/test_pages.py
@@ -1,7 +1,6 @@
 from pytest import mark
 
 from django.shortcuts import reverse
-from django.test import override_settings
 
 from tests import factories
 
@@ -9,8 +8,10 @@ from tests import factories
 pytestmark = mark.django_db
 
 
-@override_settings(DEBUG=True, ALLOW_DEMO_LOGIN=True, DEMO_INSPECTOR_USERNAME='inspector@test.com')
-def test_demo_user_is_logged_in_when_requesting_demo_page(client):
+def test_demo_user_is_logged_in_when_requesting_demo_page(client, settings):
+    settings.DEBUG = True
+    settings.ALLOW_DEMO_LOGIN = True
+    settings.DEMO_INSPECTOR_USERNAME = 'inspector@test.com'
     factories.UserFactory(username='inspector@test.com', is_superuser=False, is_staff=False)
     response = client.get(reverse('demo-inspector'))
     assert response.status_code == 302
@@ -19,8 +20,10 @@ def test_demo_user_is_logged_in_when_requesting_demo_page(client):
     assert session_user.is_authenticated
 
 
-@override_settings(DEBUG=False, ALLOW_DEMO_LOGIN=True, DEMO_INSPECTOR_USERNAME='inspector@test.com')
-def test_login_as_demo_user_is_not_available_if_debug_mode_if_off(client):
+def test_login_as_demo_user_is_not_available_if_debug_mode_if_off(client, settings):
+    settings.DEBUG = False
+    settings.ALLOW_DEMO_LOGIN = True
+    settings.DEMO_INSPECTOR_USERNAME = 'inspector@test.com'
     factories.UserFactory(username='inspector@test.com', is_superuser=False, is_staff=False)
     response = client.get(reverse('demo-inspector'))
     assert response.status_code == 404
@@ -29,8 +32,10 @@ def test_login_as_demo_user_is_not_available_if_debug_mode_if_off(client):
     assert not session_user.is_authenticated
 
 
-@override_settings(DEBUG=True, ALLOW_DEMO_LOGIN=False, DEMO_INSPECTOR_USERNAME='inspector@test.com')
-def test_login_as_demo_user_is_not_available_if_setting_prevents(client):
+def test_login_as_demo_user_is_not_available_if_setting_prevents(client, settings):
+    settings.DEBUG = True
+    settings.ALLOW_DEMO_LOGIN = False
+    settings.DEMO_INSPECTOR_USERNAME = 'inspector@test.com'
     factories.UserFactory(username='inspector@test.com', is_superuser=False, is_staff=False)
     response = client.get(reverse('demo-inspector'))
     assert response.status_code == 404
@@ -39,8 +44,10 @@ def test_login_as_demo_user_is_not_available_if_setting_prevents(client):
     assert not session_user.is_authenticated
 
 
-@override_settings(DEBUG=True, ALLOW_DEMO_LOGIN=True, DEMO_INSPECTOR_USERNAME='inspector@test.com')
-def test_demo_user_is_not_logged_in_if_superuser(client):
+def test_demo_user_is_not_logged_in_if_superuser(client, settings):
+    settings.DEBUG = True
+    settings.ALLOW_DEMO_LOGIN = True
+    settings.DEMO_INSPECTOR_USERNAME = 'inspector@test.com'
     user = factories.UserFactory(username='inspector@test.com')
     user.is_superuser = True
     user.is_staff = False
@@ -52,8 +59,10 @@ def test_demo_user_is_not_logged_in_if_superuser(client):
     assert not session_user.is_authenticated
 
 
-@override_settings(DEBUG=True, ALLOW_DEMO_LOGIN=True, DEMO_INSPECTOR_USERNAME='inspector@test.com')
-def test_demo_user_is_not_logged_in_if_staff(client):
+def test_demo_user_is_not_logged_in_if_staff(client, settings):
+    settings.DEBUG = True
+    settings.ALLOW_DEMO_LOGIN = True
+    settings.DEMO_INSPECTOR_USERNAME = 'inspector@test.com'
     user = factories.UserFactory(username='inspector@test.com')
     user.is_superuser = False
     user.is_staff = True
