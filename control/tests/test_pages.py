cdea78efdc98962b185a29a63c12d21e58e2b3c8
Estelle Comment <estelle.comment@beta.gouv.fr>
2019-06-14 09:23:29 +0200

    Restrict access to questionnaire-create url

--- control/tests/test_pages.py
+++ control/tests/test_pages.py
@@ -3,9 +3,25 @@ from pytest import mark
 from django.shortcuts import reverse
 
 from tests import factories, utils
-
+from user_profiles.models import UserProfile
 
 pytestmark = mark.django_db
+
+
+def make_inspector_user(control=None):
+    user_profile = factories.UserProfileFactory(profile_type=UserProfile.INSPECTOR)
+    if control is not None:
+        user_profile.controls.add(control)
+    user_profile.save()
+    return user_profile.user
+
+
+def make_audited_user(control=None):
+    user_profile = factories.UserProfileFactory(profile_type=UserProfile.AUDITED)
+    if control is not None:
+        user_profile.controls.add(control)
+    user_profile.save()
+    return user_profile.user
 
 
 def test_can_access_questionnaire_page_if_control_is_associated_with_the_user(client):
@@ -30,3 +46,30 @@ def test_no_access_to_questionnaire_page_if_control_is_not_associated_with_the_u
     url = reverse('questionnaire-detail', args=[questionnaire.id])
     response = client.get(url)
     assert response.status_code != 200
+
+
+def test_can_access_questionnaire_page_if_control_is_associated_with_the_inspector_user(client):
+    control = factories.ControlFactory()
+    user = make_inspector_user(control)
+    utils.login(client, user=user)
+    url = reverse('questionnaire-create', args=[control.id])
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+def test_no_access_questionnaire_page_if_control_is_not_associated_with_the_inspector_user(client):
+    control = factories.ControlFactory()
+    user = make_inspector_user()  # not associated with control
+    utils.login(client, user=user)
+    url = reverse('questionnaire-create', args=[control.id])
+    response = client.get(url)
+    assert 400 <= response.status_code < 500
+
+
+def test_no_access_questionnaire_page_if_not_inspector_user(client):
+    control = factories.ControlFactory()
+    user = make_audited_user(control)
+    utils.login(client, user=user)
+    url = reverse('questionnaire-create', args=[control.id])
+    response = client.get(url)
+    assert 400 <= response.status_code < 500
