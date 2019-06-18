53b3d98ba65eb77f17dbd00fbe81b1be51a9e8fd
Estelle Comment <estelle.comment@gmail.com>
2019-06-18 18:20:42 +0200

    Add help tooltip. Position is weird but it works.

---
+++ static/src/utils/HelpTooltip.vue
@@ -0,0 +1,24 @@
+<template>
+  <span>
+    <a href="javascript:void(0);" data-toggle="tooltip" :title="text">
+      <i class="fe fe-help-circle"></i>
+    </a>
+
+    <div class="tooltip bs-tooltip-top" role="tooltip">
+      <div class="arrow"></div>
+      <div class="tooltip-inner">
+        {{ text }}
+      </div>
+    </div>
+</span>
+
+</template>
+
+<script>
+  import Vue from "vue"
+
+  export default Vue.extend({
+    props: [ 'text' ]
+  })
+
+</script>
