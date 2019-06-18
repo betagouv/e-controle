8b9d4b5e0d24f3ed9f7ecd942f6ca5f3b794df1c
Estelle Comment <estelle.comment@gmail.com>
2019-06-18 17:22:08 +0200

    Add confirm modal for creating questionnaire

--- static/src/questionnaires/QuestionnairePreview.vue
+++ static/src/questionnaires/QuestionnairePreview.vue
@@ -13,15 +13,30 @@
       <a href="javascript:void(0)" @click.prevent="back()" class="btn btn-link">
         < Retour
       </a>
-      <button type="submit" @click.prevent="done()" class="btn btn-primary ml-auto">
-        Confirmer la création
+      <button type="submit"
+              data-toggle="modal"
+              data-target="#saveQuestionnaireConfirmModal"
+              class="btn btn-primary ml-auto"
+              title="Publier le questionnaire à l'organisme interrogé">
+        Publier
       </button>
     </div>
+
+    <confirm-modal id="saveQuestionnaireConfirmModal"
+                   title="Confirmer la publication"
+                   confirm-button="Oui, j'ai compris"
+                   cancel-button="Retour"
+                   @confirm="done()"
+    >
+      En publiant ce questionnaire, vous allez le rendre visible à l'organisme interrogé, et vous ne pourrez plus le modifier.
+    </confirm-modal>
+
   </div>
 </template>
 
 <script>
   import Vue from "vue"
+  import ConfirmModal from "../utils/ConfirmModal"
   import QuestionnaireDetail from "./QuestionnaireDetail"
 
   export default Vue.extend({
@@ -56,6 +71,7 @@
       },
     },
     components: {
+      ConfirmModal,
       QuestionnaireDetail
     }
   });
