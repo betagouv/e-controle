ac6aa8fa71303434ff9707af534476e31241c24e
Estelle Comment <estelle.comment@gmail.com>
2019-06-19 16:12:04 +0200

    Intermediate commit : create delete func, not using it yet

--- control/api_views.py
+++ control/api_views.py
@@ -95,25 +95,65 @@ class QuestionnaireViewSet(viewsets.ModelViewSet):
 
         return serializer.validated_data.get('themes', [])
 
-    def __save_themes_and_questions(self, saved_qr, validated_themes_and_questions, user, verb):
-        def find_child_obj_by_id(parent_obj, obj_id, obj_class):
-            """
-            If obj_id is in parent_obj's children, return the corresponding object. Else return none.
-            :param parent_obj: e.g. an instance of Questionnaire
-            :param obj_id: e.g. 3
-            :param obj_class: e.g. Theme
-            :return:
-            """
-            if parent_obj is None or obj_id is None:
-                return None
-            child_class_name_plural = obj_class.__name__.lower() + 's'
+    def __delete_objects_not_in_request_data(self, qr_in_db, themes_request_data, user):
+        """
+        This is for an questionnaire update request.
+        If the request data contains themes 1 and 3, while the corresponding questionnaire in DB contains themes 1, 2
+        and 3, this function deletes theme 2 from DB.
+        Same thing with questions.
+        Note that in request data, themes and questions may have an id (if the corresponding DB object should be
+        updated), or not (if there is no DB object yet, and it should be created).
+        :param qr_in_db: questionnaire currently in DB.
+        :param themes_request_data: list of themes coming from update request.
+        :return:
+        """
+        def log_delete(saved_object):
+            self.__log_action(user, 'deleted', saved_object, qr_in_db.control)
 
-            children = getattr(parent_obj, child_class_name_plural)
-            children_ids = children.all().values_list('id')
-            if children_ids.filter(id=obj_id).exists():
-                return obj_class.objects.get(id=obj_id)
+        def __find_child_theme_by_id(parent_questionnaire, theme_id):
+            return self.__find_child_obj_by_id(parent_questionnaire, theme_id, Theme)
+
+        # remove themes from DB if they aren't in request data.
+        theme_ids_in_request = [theme_request_data.get('id') for theme_request_data in themes_request_data]
+        for theme_in_db in qr_in_db.themes.all():
+            if theme_in_db.id not in theme_ids_in_request:
+                theme_in_db.delete()
+                log_delete(theme_in_db)
+
+        for theme_request_data in themes_request_data:
+            # Find theme in DB corresponding to theme in request_data.
+            theme_in_db = __find_child_theme_by_id(qr_in_db, theme_request_data.get('id'))
+            if theme_in_db is None:
+                continue
+
+            questions_request_data = theme_request_data.get('questions', [])
+            question_ids_in_request = \
+                [question_request_data.get('id') for question_request_data in questions_request_data]
+            for question_in_db in theme_in_db.questions.all():
+                # if question not in request : delete it.
+                if question_in_db.id not in question_ids_in_request:
+                    question_in_db.delete()
+                    log_delete(question_in_db)
+
+    def __find_child_obj_by_id(self, parent_obj, obj_id, obj_class):
+        """
+        If obj_id is in parent_obj's children, return the corresponding object. Else return none.
+        :param parent_obj: e.g. an instance of Questionnaire
+        :param obj_id: e.g. 3
+        :param obj_class: e.g. Theme
+        :return:
+        """
+        if parent_obj is None or obj_id is None:
             return None
+        child_class_name_plural = obj_class.__name__.lower() + 's'
 
+        children = getattr(parent_obj, child_class_name_plural)
+        children_ids = children.all().values_list('id')
+        if children_ids.filter(id=obj_id).exists():
+            return obj_class.objects.get(id=obj_id)
+        return None
+
+    def __save_themes_and_questions(self, saved_qr, validated_themes_and_questions, user, verb):
         def log(saved_object):
             self.__log_action(user, verb, saved_object, saved_qr.control)
 
@@ -130,7 +170,7 @@ class QuestionnaireViewSet(viewsets.ModelViewSet):
         # add themes that are in request
         for theme_data in validated_themes_and_questions:
             # save theme
-            pre_existing_theme = find_child_obj_by_id(saved_qr, theme_data.get('id', None), Theme)
+            pre_existing_theme = self.__find_child_obj_by_id(saved_qr, theme_data.get('id', None), Theme)
             theme_ser = ThemeSerializer(pre_existing_theme, data=theme_data)
             theme_ser.is_valid(raise_exception=True)
             saved_theme = theme_ser.save(questionnaire=saved_qr)
@@ -146,7 +186,7 @@ class QuestionnaireViewSet(viewsets.ModelViewSet):
 
             # add questions that are in request
             for question_data in questions_data:
-                pre_existing_question = find_child_obj_by_id(saved_theme, question_data.get('id', None), Question)
+                pre_existing_question = self.__find_child_obj_by_id(saved_theme, question_data.get('id', None), Question)
                 question_ser = QuestionSerializer(pre_existing_question, data=question_data)
                 question_ser.is_valid(raise_exception=True)
                 saved_question = question_ser.save(theme=saved_theme)
