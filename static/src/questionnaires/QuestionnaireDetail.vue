1841204f160283ff3a15a2dcb4ad7a3f8dc894bd
Sylvain <sylvain@lazybird.co>
2019-06-27 13:27:00 +0200

    Form wizard: publish step is not clickable + fix a layout issue

--- static/src/questionnaires/QuestionnaireDetail.vue
+++ static/src/questionnaires/QuestionnaireDetail.vue
@@ -1,18 +1,5 @@
 <template>
   <div>
-    <div class="card-header pl-0">
-      <button type="button" class="btn btn-sm btn-success mr-2" style="cursor: Text;" disabled>Etape 1/4: Créer le questionnaire<i class="fa fa-angle-double-right ml-4"></i></button>
-      <button type="button" class="btn btn-sm btn-success mr-2" style="cursor: Text;" disabled>Etape 2/4: Ajouter des questions<i class="fa fa-angle-double-right ml-4"></button>
-      <button type="button" class="btn btn-sm btn-success mr-2" style="cursor: Text;" disabled>Etape 3/4: Pré-visualiser<i class="fa fa-angle-double-right ml-4"></button>
-      <button type="button"
-              class="btn btn-sm btn-success"
-              data-toggle="modal"
-              data-target="#saveQuestionnaireConfirmModal"
-              title="Publier le questionnaire à l'organisme interrogé">
-        Etape 4/4: Publier
-        <i class="fa fe fe-check ml-4">
-      </button>
-    </div>
     <div>
       <div id="metadata" class="card">
         <div class="card-body">
--- static/src/questionnaires/QuestionnairePreview.vue
+++ static/src/questionnaires/QuestionnairePreview.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+      <div class="card-header pl-0">
+        <button type="button" class="btn btn-sm btn-success mr-2" style="cursor: Text;" disabled>Etape 1/4: Créer le questionnaire<i class="fa fa-angle-double-right ml-4"></i></button>
+        <button type="button" class="btn btn-sm btn-success mr-2" style="cursor: Text;" disabled>Etape 2/4: Ajouter des questions<i class="fa fa-angle-double-right ml-4"></button>
+        <button type="button" class="btn btn-sm btn-success mr-2" style="cursor: Text;" disabled>Etape 3/4: Pré-visualiser<i class="fa fa-angle-double-right ml-4"></button>
+        <button type="button" class="btn btn-sm btn-success" style="cursor: Text;">Etape 4/4: Publier<i class="fa fe fe-check ml-4"></button>
+      </div>
     <div class="preview">
       <questionnaire-detail v-bind:questionnaire="questionnaire">
       </questionnaire-detail>
