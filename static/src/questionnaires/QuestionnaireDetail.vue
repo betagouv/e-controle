bcf10855d80e00f1bca3834eb3385c3cab486946
Sylvain <sylvain@lazybird.co>
2019-06-27 13:20:59 +0200

    Smaller form wizard buttons

--- static/src/questionnaires/QuestionnaireDetail.vue
+++ static/src/questionnaires/QuestionnaireDetail.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div class="card-header pl-0">
-      <button type="button" class="btn btn-success mr-2" style="cursor: Text;" disabled>Etape 1/4: Créer le questionnaire<i class="fa fa-angle-double-right ml-4"></i></button>
-      <button type="button" class="btn btn-success mr-2" style="cursor: Text;" disabled>Etape 2/4: Ajouter des questions<i class="fa fa-angle-double-right ml-4"></button>
-      <button type="button" class="btn btn-success mr-2" style="cursor: Text;" disabled>Etape 3/4: Pré-visualiser<i class="fa fa-angle-double-right ml-4"></button>
+      <button type="button" class="btn btn-sm btn-success mr-2" style="cursor: Text;" disabled>Etape 1/4: Créer le questionnaire<i class="fa fa-angle-double-right ml-4"></i></button>
+      <button type="button" class="btn btn-sm btn-success mr-2" style="cursor: Text;" disabled>Etape 2/4: Ajouter des questions<i class="fa fa-angle-double-right ml-4"></button>
+      <button type="button" class="btn btn-sm btn-success mr-2" style="cursor: Text;" disabled>Etape 3/4: Pré-visualiser<i class="fa fa-angle-double-right ml-4"></button>
       <button type="button"
-              class="btn btn-success"
+              class="btn btn-sm btn-success"
               data-toggle="modal"
               data-target="#saveQuestionnaireConfirmModal"
               title="Publier le questionnaire à l'organisme interrogé">
