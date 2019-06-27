f85080324ddd20a7d6d304bd7b0c8dac2b83e406
Valingot Arthur <arthur.valingot@ccomptes.fr>
2019-06-27 16:31:42 +0200

    Add app and first line of backend

---
+++ ad_authentication/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdAuthenticationConfig(AppConfig):
+    name = 'ad_authentication'
--- ecc/settings.py
+++ ecc/settings.py
@@ -201,6 +201,9 @@ REST_FRAMEWORK = {
     )
 }
 
+AUTHENTICATION_BACKENDS = ['ad_authentication.ad_backend',
+                        'django.contrib.auth.backends.ModelBackend',]
+
 CELERY_BROKER_URL = env('CELERY_BROKER_URL')
 HTTP_AUTHORIZATION = env('HTTP_AUTHORIZATION', default=None)
 
@@ -223,7 +226,10 @@ LDAP_USER = env('LDAP_USER', default=None)
 LDAP_DOMAIN = env('LDAP_DOMAIN', default=None)
 LDAP_PASSWORD = env('LDAP_PASSWORD', default=None)
 LDAP_DC = env('LDAP_DC', default=None)
+TITLE_TO_COME_IN = env('TITLE_TO_COME_IN', default='').split(',')
 
 DEMO_INSPECTOR_USERNAME = env('DEMO_INSPECTOR_USERNAME', default=None)
 DEMO_AUDITED_USERNAME = env('DEMO_AUDITED_USERNAME', default=None)
 ALLOW_DEMO_LOGIN = env('ALLOW_DEMO_LOGIN', default=False)
+
+
