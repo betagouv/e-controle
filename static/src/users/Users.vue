9c5304ccb38b7fc5c0bcc6f0c090ac02c9894d75
Sylvain <sylvain@lazybird.co>
2019-07-01 15:15:59 +0200

    Change style for the header if the user section - now using a button

--- static/src/users/Users.vue
+++ static/src/users/Users.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="card border-0 card-collapsed">
 
-    <div class="card-header border-0" data-toggle="card-collapse">
+    <div class="card-header border-0 pl-0" data-toggle="card-collapse">
       <div class="card-title">
-        <i class="fe fe-users dropdown-icon"></i>
-        <a href="#">Personnes ayant accès au contrôle</a>
+        <button type="button" class="btn btn-info"><i class="fe fe-users mr-2"></i>Personnes ayant accès au contrôle</button>
       </div>
       <div class="card-options">
         <a href="#" class="card-options-collapse"><i class="fe fe-chevron-up"></i></a>
