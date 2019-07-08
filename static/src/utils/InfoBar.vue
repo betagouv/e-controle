3f7f35537f2b1028a1099367f50f49de479d3197
Estelle Comment <estelle.comment@gmail.com>
2019-07-08 17:18:26 +0200

    InfoBar component

---
+++ static/src/utils/InfoBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="alert alert-info alert-icon alert-dismissible">
+    <button type="button" class="close" data-dismiss="alert"></button>
+    <i class="fe fe-info" aria-hidden="true"></i>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import Vue from "vue"
+
+  export default Vue.extend({})
+
+</script>
