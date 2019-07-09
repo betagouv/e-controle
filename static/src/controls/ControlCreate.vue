e5bb5e218dfdedf0622a1151d9429be22c5a38b7
Estelle Comment <estelle.comment@gmail.com>
2019-07-09 17:43:11 +0200

    Better example involving football

--- static/src/controls/ControlCreate.vue
+++ static/src/controls/ControlCreate.vue
@@ -12,16 +12,24 @@
 
     <info-bar>
       <p>
-        Vous allez créer un espace de dépôt pour votre contrôle, qui sera par la suite ouvert à l'organisme interrogé pour y déposer ses réponses.
+        Vous allez créer un espace de dépôt pour un organisme interrogé dans le cadre de votre contrôle. L'organisme interrogé pourra y déposer les réponses à vos questionnaires.
       </p>
       <p>
-        Si vous avez besoin d'interroger plusieurs organismes distincts pour un contrôle, créez un espace de dépôt pour
-        chaque organisme. Chaque organisme interrogé n'aura accès qu'à son espace.
+        Si vous avez besoin d'interroger plusieurs organismes distincts pour un même contrôle, vous pourrez créer un
+        espace de dépôt pour chaque organisme. Chaque organisme interrogé n'aura accès qu'à son espace de dépôt.
+        Ils n'auront pas accès aux questions et aux réponses des autres organismes interrogés.
       </p>
       <p>
-        Exemple : lors du contrôle de la DINSIC, vous avez besoin d'interroger la DINSIC elle-même ainsi que le bureau
-        du Premier Ministre. Vous créez un espace avec "Organisme interrogé : DINSIC", puis vous recommencez pour un second espace avec
-        "Organisme interrogé : Premier Ministre". L'équipe de la DINSIC n'aura pas accès à l'espace du Premier Ministre, et vice versa.
+        Exemple 1 : Lors du contrôle de la Fédération Française de Football (FFF), vous avez besoin d'interroger la FFF
+        elle-même ainsi que le Ministère des Sports. Vous allez créer un espace de dépôt avec "Organisme interrogé :
+        FFF", puis vous recommencez pour un second espace de dépôt avec "Organisme interrogé : Ministère des Sports".
+        L'équipe de la FFF n'aura pas accès à l'espace de dépôt du Ministère des Sports, et vice versa.
+      </p>
+      <p>
+        Exemple 2 : Pour le contrôle de la politique publique du logement, vous avez besoin d'interroger l'ADEME, puis
+        le Ministère de la Cohésion des territoires et des Relations avec les collectivités territoriales. Vous créez un
+        premier espace de dépôt avec "Organisme interrogé : ADEME", puis un second avec "Organisme interrogé : Ministère
+        de la Cohésion des territoires et des Relations avec les collectivités territoriales".
       </p>
     </info-bar>
 
@@ -29,7 +37,7 @@
       <fieldset class="form-fieldset">
         <div class="form-group mb-6">
           <label class="form-label">Nom de l’organisme interrogé<span class="form-required">*</span></label>
-          <div id="depositing-help" class="text-muted">L'organisme qui va déposer les pièces. Exemple : Premier Ministre</div>
+          <div id="depositing-help" class="text-muted">L'organisme qui va déposer les pièces. Exemple : Ministère des Sports</div>
           <input type="text" class="form-control" v-model="depositing_organization" required aria-describedby="depositing-help">
         </div>
         <div class="row">
@@ -48,8 +56,10 @@
             </div>
             <div class="col-6">
               <div class="form-group mb-6">
-                <label class="form-label">Organisme contrôlé: <span class="form-required">*</span></label>
-                <div id="control_organization-help" class="text-muted">L'organisme qui est contrôlé. Exemple : DINSIC</div>
+                <label class="form-label">Organisme ou politique contrôlé.e: <span class="form-required">*</span></label>
+                <div id="control_organization-help" class="text-muted">
+                    Exemple : FFF, ou Politique publique du logement
+                </div>
                 <input type="text" class="form-control" v-model="controlled_organization" aria-describedby="control_organization-help" required>
               </div>
             </div>
