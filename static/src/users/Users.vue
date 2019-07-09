0bb11f3a3761486d1201cd9470fa6f4154286d38
Estelle Comment <estelle.comment@gmail.com>
2019-07-09 17:30:03 +0200

    Fix bug in jumping around page

--- static/src/users/Users.vue
+++ static/src/users/Users.vue
@@ -4,7 +4,7 @@
     <div class="card-header border-0" data-toggle="card-collapse">
       <div class="card-title">
         <i class="fe fe-users dropdown-icon"></i>
-        <a href="#">Personnes ayant accès au contrôle</a>
+        <a href="">Personnes ayant accès au contrôle</a>
       </div>
       <div class="card-options">
         <a href="#" class="card-options-collapse"><i class="fe fe-chevron-up"></i></a>
