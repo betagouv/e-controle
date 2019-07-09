711680b20a3a5e3c030558348c1b14f7084fcdcb
Estelle Comment <estelle.comment@gmail.com>
2019-07-09 11:45:13 +0200

    Display error when server error. Got back to home on success.

--- static/src/controls/ControlCreate.vue
+++ static/src/controls/ControlCreate.vue
@@ -6,6 +6,10 @@
       </div>
     </div>
 
+    <error-bar v-if="hasErrors">
+      L'envoi de ce formulaire n'a pas fonctionné. Erreur : {{JSON.stringify(errors)}}
+    </error-bar>
+
     <info-bar>
       Vous pouvez créer un espace de dépôt pour chaque organisme interrogé dans le cadre de votre contrôle. Par exemple,
       pour le contrôle des comptes et de la gestion de l’Agence Z, vous pouvez créer deux espaces de dépôt distincts :
@@ -81,9 +85,11 @@
   import Vue from "vue"
 
   import ConfirmModal from "../utils/ConfirmModal"
+  import ErrorBar from "../utils/ErrorBar"
   import InfoBar from "../utils/InfoBar"
 
   const create_control_url = "/api/control/"
+  const home_url = "/accueil/"
 
   axios.defaults.xsrfCookieName = 'csrftoken'
   axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
@@ -91,31 +97,43 @@
   export default Vue.extend({
     data: function() {
       return {
-        backUrl: "/accueil/",
+        backUrl: home_url,
         organization: "",
         reference_code: "",
         title: "",
+        errors: "",
+        hasErrors: false,
       }
     },
     components: {
       ConfirmModal,
+      ErrorBar,
       InfoBar,
     },
     methods: {
+      clearErrors: function() {
+        this.errors = ""
+        this.hasErrors = false
+      },
       showModal: function() {
         $('#confirmModal').modal('show');
       },
       createControl: function() {
+        this.clearErrors()
+
         const payload = {
           title: this.title,
           reference_code: this.reference_code,
         }
         axios.post(create_control_url, payload)
           .then(response => {
-            console.log(response)
+            console.debug(response)
+            window.location.href = home_url
           })
           .catch((error) => {
-            console.log(error)
+            console.error(error)
+            this.errors = error.response.data
+            this.hasErrors = true
           })
       },
     }
---
+++ static/src/utils/ErrorBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="alert alert-danger alert-icon alert-dismissible">
+    <button type="button" class="close" data-dismiss="alert"></button>
+    <i class="fe fe-alert-triangle" aria-hidden="true"></i>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import Vue from "vue"
+
+  export default Vue.extend({})
+
+</script>
