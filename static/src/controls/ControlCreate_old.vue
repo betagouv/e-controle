dd48412726f2202611d63d31e9cb8cfb320dabc0
Estelle Comment <estelle.comment@gmail.com>
2019-07-10 15:57:56 +0200

    Move ControlCreate to questionnaire_list.html. Keeping old version as backup.

--- static/src/controls/ControlCreate_old.vue
+++ static/src/controls/ControlCreate_old.vue
@@ -1,25 +1,80 @@
 <template>
   <div>
-    <div class="card-header">
-      <div class="card-title">
+    <div class="page-header">
+      <div class="page-title">
         Créer un nouvel espace de dépôt
       </div>
     </div>
 
-    <div class="card-body">
-      <error-bar v-if="hasErrors">
-        L'envoi de ce formulaire n'a pas fonctionné. Erreur : {{JSON.stringify(errors)}}
-      </error-bar>
+    <error-bar v-if="hasErrors">
+      L'envoi de ce formulaire n'a pas fonctionné. Erreur : {{JSON.stringify(errors)}}
+    </error-bar>
 
-      <info-bar>
-        Bloup
-      </info-bar>
+    <info-bar>
+      <p>
+        Vous allez créer un espace de dépôt pour un organisme interrogé dans le cadre de votre contrôle. L'organisme interrogé pourra y déposer les réponses à vos questionnaires.
+      </p>
+      <p>
+        Si vous avez besoin d'interroger plusieurs organismes distincts pour un même contrôle, vous pourrez créer un
+        espace de dépôt pour chaque organisme. Chaque organisme interrogé n'aura accès qu'à son espace de dépôt.
+        Ils n'auront pas accès aux questions et aux réponses des autres organismes interrogés.
+      </p>
+      <p>
+        Exemple 1 : Lors du contrôle de la Fédération Française de Football (FFF), vous avez besoin d'interroger la FFF
+        elle-même ainsi que le Ministère des Sports. Vous allez créer un espace de dépôt avec "Organisme interrogé :
+        FFF", puis vous recommencez pour un second espace de dépôt avec "Organisme interrogé : Ministère des Sports".
+        L'équipe de la FFF n'aura pas accès à l'espace de dépôt du Ministère des Sports, et vice versa.
+      </p>
+      <p>
+        Exemple 2 : Pour le contrôle de la politique publique du logement, vous avez besoin d'interroger l'ADEME, puis
+        le Ministère de la Cohésion des territoires et des Relations avec les collectivités territoriales. Vous créez un
+        premier espace de dépôt avec "Organisme interrogé : ADEME", puis un second avec "Organisme interrogé : Ministère
+        de la Cohésion des territoires et des Relations avec les collectivités territoriales".
+      </p>
+    </info-bar>
 
-      <form @submit.prevent="showModal">
+    <form @submit.prevent="showModal">
+      <fieldset class="form-fieldset">
         <div class="form-group mb-6">
           <label class="form-label">Nom de l’organisme interrogé<span class="form-required">*</span></label>
           <div id="depositing-help" class="text-muted">L'organisme qui va déposer les pièces. Exemple : Ministère des Sports</div>
           <input type="text" class="form-control" v-model="depositing_organization" required aria-describedby="depositing-help">
+        </div>
+        <div class="row">
+            <div class="col-6">
+              <div class="form-group mb-6">
+                <label class="form-label">Type de contrôle : <span class="form-required">*</span></label>
+                <div id="control_type-help" class="text-muted">Exemple : CCG - Contrôle des Comptes et de la Gestion </div>
+                <select class="form-control"
+                        v-model="control_type"
+                        required
+                        aria-describedby="control_type-help">
+                  <option value="">Type de contrôle</option>
+                  <option v-for="t in control_types" :value="t.code">{{ t.code + ' - ' + t.name }}</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="form-group mb-6">
+                <label class="form-label">Organisme ou politique contrôlé.e: <span class="form-required">*</span></label>
+                <div id="control_organization-help" class="text-muted">
+                    Exemple : FFF, ou Politique publique du logement
+                </div>
+                <input type="text" class="form-control" v-model="controlled_organization" aria-describedby="control_organization-help" required>
+              </div>
+            </div>
+          </div>
+        <div class="form-group mb-6">
+          <label class="form-label">Année d'ouverture du contrôle<span class="form-required">*</span></label>
+          <div id="year-help" class="text-muted">Exemple : 2019</div>
+          <select class="form-control"
+                  v-model="year"
+                  required
+                  aria-describedby="year-help"
+                  style="width: 5em;">
+            <option value="">Année d'ouverture du contrôle</option>
+            <option v-for="y in 30" :value="y + 2015">{{ y + 2015 }}</option>
+          </select>
         </div>
 
         <div class="form-group mb-6">
@@ -37,17 +92,18 @@
           </div>
         </div>
 
-        <div class="text-right">
-          <a :href="backUrl" class="btn btn-secondary">
-            Annuler
-          </a>
-          <button type="submit"
-                  class="btn btn-primary">
-            Créer l'espace de dépôt
-          </button>
-        </div>
-      </form>
-    </div>
+
+      </fieldset>
+      <div class="text-right">
+        <a :href="backUrl" class="btn btn-secondary">
+          Annuler
+        </a>
+        <button type="submit"
+                class="btn btn-primary">
+          Créer l'espace de dépôt
+        </button>
+      </div>
+    </form>
 
     <confirm-modal id="confirmModal"
                title="Confirmer la création d'un espace de dépôt"
