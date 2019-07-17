0792a69720b1c2b6d36d067711cb445468e234d7
Estelle Comment <estelle.comment@gmail.com>
2019-07-17 14:56:22 +0200

    WebdavTip vue component replacing the django include.

--- static/src/questionnaire-list.js
+++ static/src/questionnaire-list.js
@@ -5,47 +5,31 @@ import Vuex from 'vuex'
 
 import { store } from "./store"
 import AddUserModal from "./users/AddUserModal"
-import Clipboard from 'v-clipboard'
-import CollapsibleSection from './utils/CollapsibleSection'
 import ColorBar from "./utils/ColorBar"
 import ControlCreate from "./controls/ControlCreate"
 import HelpTooltip from "./utils/HelpTooltip"
 import RemoveUserModal from "./users/RemoveUserModal"
 import UpdateUserModal from "./users/UpdateUserModal"
 import Users from './users/Users'
+import WebdavTip from './controls/WebdavTip'
 
-Vue.use(Clipboard)
 Vue.use(Vuex);
 
 new Vue({
   store,
   el: '#questionnaire-list-vm',
-  data: {
-    showCopySuccess: false
-  },
   components: {
     'users-for-control': Users,
     AddUserModal,
-    CollapsibleSection,
     ColorBar,
     ControlCreate,
     UpdateUserModal,
     RemoveUserModal,
-    HelpTooltip
+    HelpTooltip,
+    WebdavTip,
   },
   methods: {
     ...mapActions(['setSessionUser']),
-
-    clipboardSuccessHandler ({ value, event }) {
-      this.showCopySuccess = true
-    },
-
-    enterFade: function(el, done) {
-      var that = this;
-      setTimeout(function() {
-        that.showCopySuccess = false;
-      }, 3000); // hide the message after 3 seconds
-    }
   },
   created() {
     this.setSessionUser()
