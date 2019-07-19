fd5e509e3279b3affc638c60d0aadd72bb31116b
Estelle Comment <estelle.comment@gmail.com>
2019-07-19 14:30:17 +0200

    Remove unused functions

--- control/copyQuestionFile.py
+++ control/copyQuestionFile.py
@@ -3,29 +3,6 @@ from django.core.files import File
 from control.models import Control, Question, Questionnaire, QuestionFile, Theme
 
 
-# Unused
-def find_ids_of_questions_to_copy_to(existing_question_id):
-    """
-    Find questions with the same description.
-    """
-    existing_question = Question.objects.get(id=existing_question_id)
-    ids = Question.objects.filter(description=existing_question.description).values_list('id', flat=True)
-    return list(ids)
-
-
-# Unused
-def copy_all_the_things(existing_question_file_id):
-    """
-    Copy the given QuestionFile to all Questions with same description as the given Question.
-    """
-    existing_question_id = QuestionFile.objects.get(id=existing_question_file_id).question_id
-
-    ids_of_questions_to_copy_to = find_ids_of_questions_to_copy_to(existing_question_id)
-
-    for id_of_question_to_copy_to in ids_of_questions_to_copy_to:
-        copy_question_file(existing_question_file_id, id_of_question_to_copy_to)
-
-
 def copy_question_file(existing_question_file_id, id_of_question_to_copy_to):
     """
     Copy an existing QuestionFile and add it to a given question.
