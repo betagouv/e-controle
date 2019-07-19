54fbcb9d8f4c3cb47abab866b68c6bc15a506dc0
Estelle Comment <estelle.comment@gmail.com>
2019-07-19 12:15:13 +0200

    Copy questionnaires

--- control/copyQuestionFile.py
+++ control/copyQuestionFile.py
@@ -1,6 +1,6 @@
 from django.core.files import File
 
-from control.models import Question, QuestionFile
+from control.models import Control, Question, Questionnaire, QuestionFile, Theme
 
 
 def copy_question_file(existing_question_file_id, id_of_question_to_copy_to):
@@ -34,4 +34,35 @@ def copy_all_the_things(existing_question_file_id):
 
     for id_of_question_to_copy_to in ids_of_questions_to_copy_to:
         copy_question_file(existing_question_file_id, id_of_question_to_copy_to)
+
+
+def copyQuestionnaire(existing_questionnaire_id, id_of_control_to_copy_to):
+    existing_questionnaire = Questionnaire.objects.get(id=existing_questionnaire_id)
+    control_to_copy_to = Control.objects.get(id=id_of_control_to_copy_to)
+
+    existing_questionnaire.id = None
+    existing_questionnaire.control = control_to_copy_to
+    # TODO order field is copied along
+    existing_questionnaire.save()
+    new_questionnaire_id = existing_questionnaire.id
+
+    # Fetch again to get the full objects.
+    existing_questionnaire = Questionnaire.objects.get(id=existing_questionnaire_id)
+    new_questionnaire = Questionnaire.objects.get(id=new_questionnaire_id)
+
+    for theme in existing_questionnaire.themes.all():
+        existing_theme_id = theme.id
+        theme.id = None
+        theme.questionnaire = new_questionnaire
+        theme.save()
+        new_theme_id = theme.id
+
+        existing_theme = Theme.objects.get(id=existing_theme_id)
+        new_theme = Theme.objects.get(id=new_theme_id)
+        for question in existing_theme.questions.all():
+            existing_question_id = question.id
+            question.id = None
+            question.theme = new_theme
+            question.save()
+            new_question_id = question.id
 
