8f473478e9939d23cb090720b71236fc8b8d394c
Estelle Comment <estelle.comment@gmail.com>
2019-07-19 11:42:23 +0200

    Fetch question id

--- control/copyQuestionFile.py
+++ control/copyQuestionFile.py
@@ -1,6 +1,6 @@
 from django.core.files import File
 
-from .models import Question, QuestionFile
+from control.models import Question, QuestionFile
 
 
 def copy_question_file(existing_question_file_id, id_of_question_to_copy_to):
@@ -24,17 +24,14 @@ def get_ids_of_questions_to_copy_to(existing_question_id):
     return list(ids)
 
 
-def copy_all_the_things(existing_question_id, existing_question_file_id):
+def copy_all_the_things(existing_question_file_id):
     """
     Copy the given QuestionFile to all Questions with same description as the given Question.
     """
+    existing_question_id = QuestionFile.objects.get(id=existing_question_file_id).question_id
+
     ids_of_questions_to_copy_to = get_ids_of_questions_to_copy_to(existing_question_id)
 
     for id_of_question_to_copy_to in ids_of_questions_to_copy_to:
         copy_question_file(existing_question_file_id, id_of_question_to_copy_to)
-
-
-existing_question_id = 107
-existing_question_file_id = 24
-copy_all_the_things(existing_question_id, existing_question_file_id)
 
