b50db897b323f4b1b524ec99a9c95ef39b80f41a
Estelle Comment <estelle.comment@gmail.com>
2019-07-24 19:37:30 +0200

    QuestionnaireDetailPage comp

---
+++ static/src/questionnaires/QuestionnaireDetailPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    Got questionnaire: {{ questionnaire.title }}
+  </div>
+
+</template>
+
+<script>
+  import Vue from "vue";
+
+  import axios from 'axios'
+
+  const questionnaire_url = "/api/questionnaire/";
+
+  export default Vue.extend({
+    props: [ 'questionnaireId' ],
+    data : function () {
+      return {
+        questionnaire: ''
+      }
+    },
+    mounted: function(){
+      this.getQuestionnaire()
+    },
+    methods: {
+      getQuestionnaire: function() {
+        axios.get(questionnaire_url + this.questionnaireId).then(response => {
+          this.questionnaire = response.data
+        });
+      },
+    },
+  })
+
+</script>
