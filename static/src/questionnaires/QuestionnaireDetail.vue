e25cb774bed199ac1df2dbe88f0612fcf4eb6ccc
Estelle Comment <estelle.comment@gmail.com>
2019-07-24 18:03:15 +0200

    Refactor out QuestionnaireMetadata

--- static/src/questionnaires/QuestionnaireDetail.vue
+++ static/src/questionnaires/QuestionnaireDetail.vue
@@ -1,38 +1,7 @@
 <template>
   <div>
-    <div id="metadata" class="card">
-      <div class="card-body">
-        <p class="with-line-breaks">{{ questionnaire.description }}</p>
-        <p v-if="questionnaire.sent_date">
-          <i class="fe fe-send"></i>
-          Date de transmission du questionnaire :
-          {{ questionnaire.sent_date}}
-        </p>
-
-        <p v-if="questionnaire.end_date">
-          <i class="fe fe-clock"></i>
-          Date de réponse souhaitée :
-          {{ questionnaire.end_date | DateFormat}}
-        </p>
-
-        <div class="text-right" v-if="questionnaire.id">
-          <a :href="'/fichier-questionnaire/' + questionnaire.id" target="_blank">
-            <button type="submit" class="btn btn-primary" title="Exporter ce questionnaire">
-              <i class="fe fe-file-text mr-2"></i>
-              Exporter ce questionnaire
-            </button>
-          </a>
-        </div>
-
-        <div class="text-right" v-else>
-            <button type="submit" class="btn btn-primary" title="Exporter ce questionnaire" disabled>
-              <i class="fe fe-file-text mr-2"></i>
-              Exporter ce questionnaire
-            </button>
-          <div class="small text-muted">*Vous devez d'abord enregistrer votre brouillon.</div>
-        </div>
-      </div>
-    </div>
+    <questionnaire-metadata :questionnaire="questionnaire">
+    </questionnaire-metadata>
 
     <div id="body" class="row row-cards">
       <div id="sidebar" class="col-lg-4">
@@ -88,6 +57,7 @@
   import DateFormat from '../utils/DateFormat.js';
   import Question from '../details/Question.vue';
   import QuestionFileList from "./QuestionFileList"
+  import QuestionnaireMetadata from './QuestionnaireMetadata'
 
   export default Vue.extend({
     props: ['questionnaire'],
@@ -99,7 +69,8 @@
     },
     components: {
       Question,
-      QuestionFileList
+      QuestionFileList,
+      QuestionnaireMetadata,
     }
   });
 </script>
