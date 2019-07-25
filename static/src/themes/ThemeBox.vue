1b87be2d0549b1bdf1a418b0ef9d556425baa809
Estelle Comment <estelle.comment@gmail.com>
2019-07-25 15:56:26 +0200

    Add component ThemeBox

---
+++ static/src/themes/ThemeBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-bind:id="'theme' + themeNumbering"
+       class="card">
+    <div class="card-status card-status-top bg-blue"></div>
+    <div class="card-header">
+      <h3 class="card-title">{{ themeNumbering }}. {{ theme.title }}</h3>
+    </div>
+
+    <slot></slot>
+
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    props: [
+      'theme',
+      'themeNumbering',
+    ],
+  })
+</script>
