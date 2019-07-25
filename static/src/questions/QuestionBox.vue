922b43ecfa8dcbd39990393a73210c0ba34ea2e1
Estelle Comment <estelle.comment@gmail.com>
2019-07-25 15:24:17 +0200

    Remove unused old props

--- static/src/questions/QuestionBox.vue
+++ static/src/questions/QuestionBox.vue
@@ -2,18 +2,10 @@
   <div v-bind:id="'question' + themeNumbering + '.' + questionNumbering"
        class="card m-0 p-0 pb-0">
     <div class="card-header border-0" data-toggle="card-collapse">
-      <question v-if="question"
-                :question-description="question.description"
+      <question :question-description="question.description"
                 :theme-numbering="themeNumbering"
                 :question-numbering="questionNumbering"
                 :question-id="question.id">
-
-      </question>
-      <question v-else
-                :question-description="questionDescription"
-                :theme-numbering="themeNumbering"
-                :question-numbering="questionNumbering"
-                :question-id="questionId">
       </question>
     </div>
     <div class="card-body">
@@ -32,11 +24,6 @@
       'question',
       'questionNumbering',
       'themeNumbering',
-
-      // todo remove these 3 and the v-else when we switch to all-Vue
-      'questionFileCount',
-      'questionId',
-      'questionDescription',
     ],
     components: {
       Question,
