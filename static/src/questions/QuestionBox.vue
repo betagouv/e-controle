ac8bad21170196b7a3cf22350f63c84d99e80118
Estelle Comment <estelle.comment@gmail.com>
2019-07-26 10:58:07 +0200

    Use bootstrap collapse instead of card-collapse, which doesnt work inside a v-for.

--- static/src/questions/QuestionBox.vue
+++ static/src/questions/QuestionBox.vue
@@ -1,13 +1,13 @@
 <template>
   <div v-bind:id="'question' + themeNumbering + '.' + questionNumbering"
        class="card m-0 p-0 pb-0">
-    <div class="card-header border-0" data-toggle="card-collapse">
+    <div class="card-header border-0" data-toggle="collapse" :data-target="'#question-body-' + question.id">
       <question :theme-numbering="themeNumbering"
                 :question-numbering="questionNumbering"
                 :question="question">
       </question>
     </div>
-    <div class="card-body">
+    <div class="card-body collapse" :id="'question-body-' + question.id">
       <slot></slot>
     </div>
 
