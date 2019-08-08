f3100fe0723401b83c043069bf68124be3980794
Estelle Comment <estelle.comment@gmail.com>
2019-08-08 11:15:00 +0200

    Remove unneeded stuff from factory in tests

--- magicauth/tests/factories.py
+++ magicauth/tests/factories.py
@@ -10,13 +10,8 @@ faker = FakerFactory.create('fr_FR')
 
 @register
 class UserFactory(factory.DjangoModelFactory):
-    first_name = factory.LazyFunction(faker.first_name)
-    last_name = factory.LazyFunction(faker.last_name)
     email = factory.LazyFunction(faker.email)
     username = factory.LazyAttribute(lambda a: a.email)
-    password = factory.PostGenerationMethodCall('set_password', '123')
-    is_active = True
-    is_staff = True
 
     class Meta:
         model = get_user_model()
