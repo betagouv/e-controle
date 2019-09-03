cdad06fe57928e03900aa28429531d581c4b8812
Sylvain <sylvain@lazybird.co>
2019-09-03 15:55:41 +0200

    The API now sends the sesstion timeout setting: seconds before expire

--- session/api_views.py
+++ session/api_views.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.conf import settings
 
 from rest_framework import decorators
@@ -12,16 +10,16 @@ SESSION_TIMEOUT_KEY = "_session_init_timestamp_"
 
 class SessionTimeoutViewSet(viewsets.ViewSet):
 
-    @decorators.action(detail=False, methods=['get'], url_path='expire-time')
-    def expire_time(self, request):
+    @decorators.action(detail=False, methods=['get'], url_path='get-timeout')
+    def get_timeout(self, request):
         expire_seconds = getattr(
             settings, "SESSION_EXPIRE_SECONDS", settings.SESSION_COOKIE_AGE
         )
-        last_activity_timestamp = request.session.get(SESSION_TIMEOUT_KEY)
-        expire_timestamp = last_activity_timestamp + expire_seconds
-        expire_time = datetime.fromtimestamp(expire_timestamp)
-        return Response({'expire': expire_time})
+        return Response({
+            'backend_expire': expire_seconds,
+            'frontend_expire': round(expire_seconds - expire_seconds*0.1),
+        })
 
     @decorators.action(detail=False, methods=['get'], url_path='keep-alive')
     def keep_alive(self, request):
-        return self.expire_time(request)
+        return self.get_timeout(request)
