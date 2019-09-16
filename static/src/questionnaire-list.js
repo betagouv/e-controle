1cc248adf145a453bb6b2c21402560d2651b8e55
Estelle Comment <estelle.comment@gmail.com>
2019-09-16 14:55:01 +0200

    WIP hookup the backend for publish button in questionnaire-list

--- static/src/questionnaire-list.js
+++ static/src/questionnaire-list.js
@@ -1,11 +1,12 @@
 import "@babel/polyfill"
 
 import { mapActions } from 'vuex'
+import { store } from "./store"
 import Vue from 'vue/dist/vue.js'
 import Vuex from 'vuex'
 
-import { store } from "./store"
 import AddUserModal from "./users/AddUserModal"
+import axios from "axios"
 import ColorBar from "./utils/ColorBar"
 import ControlCreate from "./controls/ControlCreate"
 import ControlTitle from "./controls/ControlTitle"
@@ -17,6 +18,8 @@ import Users from './users/Users'
 import WebdavTip from './controls/WebdavTip'
 
 Vue.use(Vuex);
+
+const get_questionnaire_url = "/api/questionnaire/"
 
 new Vue({
   store,
@@ -35,6 +38,25 @@ new Vue({
   },
   methods: {
     ...mapActions(['setSessionUser']),
+    publish: function(questionnaireId) {
+      console.log('publish', questionnaireId)
+/*      axios.get(get_questionnaire_url + this.questionnaireId)
+          .then(response => {
+            console.debug('Got draft : ', response.data)
+            if (response.data.is_draft !== undefined && !response.data.is_draft) {
+              const errorMessage = 'Le questionnaire ' + response.data.id +
+                  ' n\'est pas un brouillon. Vous ne pouvez pas le modifier.'
+              this.displayErrors(errorMessage)
+              return
+            }
+            this.questionnaire = response.data
+            this.emitQuestionnaireLoaded()
+            this.emitQuestionnaireUpdated()
+            this.moveToState(STATES.START)
+          }).catch(error => {
+            this.displayErrors('Erreur lors du chargement du brouillon.', error.response.data)
+          })*/
+    },
   },
   created() {
     this.setSessionUser()
