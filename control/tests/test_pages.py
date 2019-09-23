7f8a5c802aec6bce5949e72f887b95768d76900d
Estelle Comment <estelle.comment@gmail.com>
2019-09-23 11:11:41 +0200

    Fix tests

--- control/tests/test_pages.py
+++ control/tests/test_pages.py
@@ -2,7 +2,9 @@ from pytest import mark
 
 from django.shortcuts import reverse
 
+from control.tests import test_api_questionnaire
 from tests import factories, utils
+from tests.factories import ControlFactory
 from user_profiles.models import UserProfile
 
 pytestmark = mark.django_db
@@ -93,12 +95,28 @@ def test_no_access_questionnaire_create_page_if_not_inspector_user(client):
     assert 400 <= response.status_code < 500
 
 
-def test_can_access_questionnaire_edit_page_if_control_is_associated_with_the_inspector_user(client):
+def test_can_access_questionnaire_edit_page_if_control_is_associated_with_the_inspector_user_and_user_is_author(client):
+    # Create questionnaire through API so that the author is set properly.
+    control = ControlFactory()
+    user = utils.make_user(UserProfile.INSPECTOR, control)
+    payload = test_api_questionnaire.make_create_payload(control.id)
+    create_response = test_api_questionnaire.create_questionnaire(user, payload)
+    assert create_response.status_code == 201
+    questionnaire_id = create_response.data['id']
+
+    utils.login(client, user=user)
+    url = reverse('questionnaire-edit', args=[questionnaire_id])
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+
+def test_no_access_questionnaire_edit_page_if_user_is_not_author(client):
     response = access_questionnaire_page(client,
                                          page_name='questionnaire-edit',
                                          is_control_associated_with_user=True,
                                          profile_type=UserProfile.INSPECTOR)
-    assert response.status_code == 200
+    assert 400 <= response.status_code < 500
 
 
 def test_no_access_questionnaire_edit_page_if_control_is_not_associated_with_the_inspector_user(client):
