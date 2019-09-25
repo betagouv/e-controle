14a7953602f7fe075d9b826875da5214beb78f91
Estelle Comment <estelle.comment@gmail.com>
2019-09-25 11:47:34 +0200

    Style the hover of clickable steps

--- static/src/utils/WizardStep.vue
+++ static/src/utils/WizardStep.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="wizard-step">
-    <div class="wizard-step-thread"></div>
-    <div v-if="!clickable" class="wizard-step-bubble card-title">
-      <span class="number">{{ number }}</span>
-      <i class="done-icon fe fe-check"></i>
-    </div>
-    <a v-else class="wizard-step-bubble card-title" href="javascript:void(0);" @click.prevent="$emit('clickedStep')">
-      <span class="number">{{ number }}</span>
-      <i class="done-icon fe fe-check"></i>
+
+    <a v-if="clickable"
+       class="wizard-step-graphics"
+       href="javascript:void(0);"
+       @click.prevent="$emit('clickedStep')">
+      <div class="wizard-step-thread"></div>
+      <div class="wizard-step-bubble card-title">
+        <span class="number">{{ number }}</span>
+        <i class="done-icon fe fe-check"></i>
+      </div>
     </a>
+
+    <div v-else
+         class="wizard-step-graphics">
+      <div class="wizard-step-thread"></div>
+      <div class="wizard-step-bubble card-title">
+        <span class="number">{{ number }}</span>
+        <i class="done-icon fe fe-check"></i>
+      </div>
+    </div>
+
     <slot></slot>
-  </div>
 </template>
 
 <script>
