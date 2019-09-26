1d3ca11be2ac385d0548937d2098879ee58752e1
Estelle Comment <estelle.comment@beta.gouv.fr>
2019-09-26 13:29:27 +0200

    First test for QuestionnairePreview

--- static/src/questionnaires/test/QuestionnairePreview.spec.js
+++ static/src/questionnaires/test/QuestionnairePreview.spec.js
@@ -1,9 +1,32 @@
-import {mount} from "@vue/test-utils";
+import { mount, shallowMount } from "@vue/test-utils";
+
 import QuestionnairePreview from '../QuestionnairePreview.vue'
+import Wizard from '../../utils/Wizard'
+
+const assertHasEmmitted = (obj, eventName, numTimes) => {
+  expect(obj.emitted()).toHaveProperty(eventName)
+  expect(obj.emitted()[eventName].length).toEqual(numTimes)
+}
 
 describe('QuestionnairePreview.vue', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(QuestionnairePreview)
+    // shallowMount stubs out all children
+    const wrapper = shallowMount(QuestionnairePreview)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
+
+  test('emits "back" when Wizard emits "previous"', () => {
+    // Stub out a Wizard. (Other child components are really instantiated)
+    const wrapper = mount(QuestionnairePreview, {
+      stubs: {
+        Wizard: true
+      }
+    })
+
+    const wizard = wrapper.find(Wizard).vm
+    wizard.$emit('previous')
+
+    assertHasEmmitted(wrapper, 'back', 1)
+  })
+
 })
