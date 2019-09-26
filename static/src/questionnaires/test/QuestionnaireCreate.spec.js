afac9634586ddec69f123a6ed4afa3b41a309deb
Estelle Comment <estelle.comment@beta.gouv.fr>
2019-09-26 12:51:10 +0200

    Fix test for QuestionnaireCreate.vue

--- static/src/questionnaires/test/QuestionnaireCreate.spec.js
+++ static/src/questionnaires/test/QuestionnaireCreate.spec.js
@@ -1,9 +1,9 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import QuestionnaireCreate from '../QuestionnaireCreate.vue'
 
 describe('QuestionnaireCreate.vue', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(
+    const wrapper = shallowMount(
         QuestionnaireCreate,
         {
           propsData: {
