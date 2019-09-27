b9dac519393e318cab2e011cbdc1ef87ee365ef8
Estelle Comment <estelle.comment@gmail.com>
2019-09-27 14:17:42 +0200

    More tests for QuestionnairePreview

--- static/src/questionnaires/QuestionnairePreview.vue
+++ static/src/questionnaires/QuestionnairePreview.vue
@@ -17,7 +17,7 @@
           <button type="submit" @click.prevent="back()" class="btn btn-secondary ml-auto">
             < Retour
           </button>
-          <button type="submit" @click.prevent="saveDraft" class="btn btn-primary">
+          <button id="saveDraftFromPreviewButton" type="submit" @click.prevent="saveDraft" class="btn btn-primary">
             <i class="fe fe-save mr-1"></i>
             Enregistrer le brouillon
           </button>
