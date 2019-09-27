2b8d26be7b3a42b17cb6406b7cce102c00645678
Estelle Comment <estelle.comment@gmail.com>
2019-09-27 15:06:58 +0200

    Test mounting of QuestionnaireCreate

--- static/src/questionnaires/test/QuestionnaireCreate.spec.js
+++ static/src/questionnaires/test/QuestionnaireCreate.spec.js
@@ -1,5 +1,13 @@
+import axios from 'axios'
 import { shallowMount } from '@vue/test-utils'
 import QuestionnaireCreate from '../QuestionnaireCreate.vue'
+import QuestionnaireMetadataCreate from '../QuestionnaireMetadataCreate'
+
+jest.mock('axios', () => ({
+  post: jest.fn(() => Promise.resolve({ data: 'saved-questionnaire' })),
+  get: jest.fn(() => Promise.resolve({ data: 'saved-questionnaire' })),
+  defaults: {},
+}))
 
 describe('QuestionnaireCreate.vue', () => {
   test('is a Vue instance', () => {
@@ -11,6 +19,24 @@ describe('QuestionnaireCreate.vue', () => {
           }
     })
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('crashes without a controlId or questionnaireId', () => {
+    expect(() => {
+      shallowMount(QuestionnaireCreate)
+    }).toThrow()
+  })
+
+  test('runs with controlId', () => {
+    expect(() => {
+      shallowMount(QuestionnaireCreate, { propsData: { controlId: 1}})
+    }).not.toThrow()
+  })
+
+  test('runs with questionnaireId', () => {
+    expect(() => {
+      shallowMount(QuestionnaireCreate, { propsData: { questionnaireId: 1}})
+    }).not.toThrow()
   })
 })
 
