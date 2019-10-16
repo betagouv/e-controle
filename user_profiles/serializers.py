d62c9bb43000c2bd41d86bc69323d8538e3fc1bc
Sylvain <sylvain@lazybird.co>
2019-10-16 15:24:12 +0200

    When adding/updating a user, the API expect only 1 control
    Before, we used to have multiple controls which is not needed.

--- user_profiles/serializers.py
+++ user_profiles/serializers.py
@@ -17,7 +17,8 @@ class RemoveControlSerializer(serializers.Serializer):
 
 class UserProfileSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(source='user.pk', read_only=True)
-    controls = serializers.PrimaryKeyRelatedField(many=True, queryset=Control.objects.all())
+    control = serializers.PrimaryKeyRelatedField(
+        queryset=Control.objects.all(), write_only=True, required=False)
     first_name = serializers.CharField(source='user.first_name')
     last_name = serializers.CharField(source='user.last_name')
     email = serializers.EmailField(source='user.email')
@@ -26,15 +27,18 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = UserProfile
         fields = (
             'id', 'first_name', 'last_name', 'email', 'profile_type',
-            'organization', 'controls', 'is_audited', 'is_inspector')
-        extra_kwargs = {'controls': {'write_only': True}}
+            'organization', 'control', 'is_audited', 'is_inspector')
 
     def create(self, validated_data):
         profile_data = validated_data
-        controls_data = profile_data.pop('controls')
+        control = profile_data.pop('control', None)
         user_data = profile_data.pop('user')
         user_data['username'] = user_data['email']
         profile = UserProfile.objects.filter(user__username=user_data.get('email')).first()
+        session_user = self.context['request'].user
+        if control and control not in session_user.profile.controls.all():
+            raise serializers.ValidationError(
+                f"{session_user} n'est pas authorisé à modifier ce contrôle: {control}")
         action_details = {}
         action_details['sender'] = self.context['request'].user
         should_receive_email_report = False
@@ -48,7 +52,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
             profile.send_files_report = should_receive_email_report
             profile.user.save()
             profile.save()
-            action_details['verb'] = 'updated'
+            if control and control not in profile.controls.all():
+                # The incoming control is not already associated to this user,
+                # so we know that the user is being added to this control.
+                action_details['verb'] = 'added'
+            else:
+                action_details['verb'] = 'updated'
+
         else:
             user = User.objects.create(**user_data)
             profile_data['user'] = user
@@ -56,14 +66,8 @@ class UserProfileSerializer(serializers.ModelSerializer):
             profile = UserProfile.objects.create(**profile_data)
             action_details['verb'] = 'added'
         action_details['action_object'] = profile
-        controls_to_be_added = [c for c in controls_data if c not in profile.controls.all()]
-        session_user = self.context['request'].user
-        for control in controls_to_be_added:
-            if control not in session_user.profile.controls.all():
-                raise serializers.ValidationError(
-                    f"{session_user} n'est pas authorisé à modifier ce contrôle: {control}")
+        if control:
             profile.controls.add(control)
-            action_details['verb'] = 'added'
             action_details['target'] = control
         if profile.is_inspector:
             action_details['verb'] += ' inspector user'
