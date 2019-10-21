472e8fe9af18f0c01fe11842b09ae8abe33e40b6
Sylvain <sylvain@lazybird.co>
2019-10-21 15:27:40 +0200

    Add logging to email sending

--- utils/email.py
+++ utils/email.py
@@ -3,8 +3,24 @@ from django.contrib.sites.models import Site
 from django.core.mail import EmailMultiAlternatives
 from django.template import loader
 
+from actstream import action
+
 
 current_site = Site.objects.get_current()
+
+
+def add_log_entry(site, verb, to, cc, subject, error=''):
+    log_message = f'Sending email "{subject}" to: {to}.'
+    if cc:
+        log_message += f' Email CC: {cc}.'
+    if error:
+        log_message += f' Failed with error: {error}'
+    action_details = {
+        'sender': site,
+        'verb': verb,
+        'description': log_message,
+    }
+    action.send(**action_details)
 
 
 def send_email(
@@ -23,4 +39,10 @@ def send_email(
         from_email=from_email,
     )
     email.attach_alternative(html_message, "text/html")
-    email.send(fail_silently=False)
+    try:
+        email.send(fail_silently=False)
+        add_log_entry(site=current_site, verb='email sent', to=to, cc=cc, subject=subject)
+    except Exception as e:
+        add_log_entry(
+            site=current_site, verb='email not sent', to=to, cc=cc, subject=subject,
+            error=str(e))
