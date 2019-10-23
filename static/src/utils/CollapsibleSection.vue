a31be53273f4957909bac0fb2f67b41c084c55c7
Estelle Comment <estelle.comment@gmail.com>
2019-10-23 11:22:57 +0200

    For CollapsibleSection, use bootstrap collapse instead of tabler card-collapse, it is not loading correctly with the v-ifs.

--- static/src/utils/CollapsibleSection.vue
+++ static/src/utils/CollapsibleSection.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="card border-0 mb-0 card-collapsed">
+  <div class="card border-0 mb-0">
 
-    <div class="card-header border-0 pl-0" data-toggle="card-collapse">
+    <div class="card-header border-0 pl-0"
+         data-toggle="collapse"
+         :data-target="'#' + id"
+         aria-expanded="false"
+         :aria-controls="id">
       <div class="card-title m-0">
         <button type="button" class="btn btn-secondary">
           <i class="fe mr-2" :class="buttonicon"></i>
@@ -10,7 +14,7 @@
       </div>
     </div>
 
-    <div>
+    <div class="collapse" :id="id">
       <slot></slot>
     </div>
   </div>
@@ -21,6 +25,14 @@
   import Vue from "vue"
 
   export default Vue.extend({
-    props: ['buttontext', 'buttonicon']
+    props: ['buttontext', 'buttonicon'],
+    data : function () {
+      return {
+        id: ''
+      }
+    },
+    mounted: function() {
+      this.id = 'collapsible-' + Math.floor(Math.random() * 1000000)
+    },
   })
 </script>
