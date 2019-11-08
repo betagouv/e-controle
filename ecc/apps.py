37b88154c87eed7fe8be1a5d23b5cf0c9b4e66f7
Sylvain <sylvain@lazybird.co>
2019-11-08 17:08:50 +0100

    Add logging for admin actions
    This is triggered on post save signal of the standard Django LogEntry.

--- ecc/apps.py
+++ ecc/apps.py
@@ -11,6 +11,7 @@ class EccConfig(AppConfig):
         from .celery import app as celery_app
         __all__ = ('celery_app',)
 
+        # Activity stream registration
         from actstream import registry
         registry.register(apps.get_model('control.ResponseFile'))
         registry.register(apps.get_model('control.QuestionFile'))
@@ -19,4 +20,8 @@ class EccConfig(AppConfig):
         registry.register(apps.get_model('control.Questionnaire'))
         registry.register(apps.get_model('control.Theme'))
         registry.register(apps.get_model('user_profiles.UserProfile'))
+        registry.register(apps.get_model('admin.LogEntry'))
         registry.register(get_user_model())
+
+        # Signals
+        import logs.signals  # noqa
