224e6e97643d5ec7e4dfa714a7ec624307997178
Sylvain <sylvain@lazybird.co>
2019-11-26 18:09:25 +0100

    Improuve sections where we display links to objects admin pages

--- logs/admin.py
+++ logs/admin.py
@@ -1,5 +1,9 @@
-from django.contrib import admin
 from django_admin import ReadOnlyModelAdmin
+from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
 
 from actstream.models import Action, Follow
 
@@ -8,12 +12,49 @@ admin.site.unregister(Action)
 admin.site.unregister(Follow)
 
 
+def get_admin_url(obj):
+    """
+    Return the admin page URL for the given objet
+    """
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    app_label = content_type.app_label
+    model = content_type.model
+    return reverse(f"admin:{app_label}_{model}_change", args=(obj.pk,))
+
+
+def get_object_display_line(obj):
+    """
+    For the given object, return a display line with HTML link to the admin page.
+    """
+    if not obj:
+        return '-'
+    url = get_admin_url(obj)
+    return mark_safe(f'<a href="{url}">{obj}</a>')
+
+
+def actor_display(obj):
+    return get_object_display_line(obj.actor)
+
+
+def target_display(obj):
+    return get_object_display_line(obj.target)
+
+
+def action_object_display(obj):
+    return get_object_display_line(obj.action_object)
+
+
+actor_display.short_description = 'actor'
+target_display.short_description = 'target'
+action_object_display.short_description = 'action_object'
+
+
 @admin.register(Action)
 class ActionAdmin(ReadOnlyModelAdmin, admin.ModelAdmin):
     date_hierarchy = 'timestamp'
     list_display = (
-        '__str__', 'actor', 'verb', 'target', 'target_content_type', 'action_object',
-        'action_object_content_type', 'timestamp')
+        '__str__', actor_display, 'verb', target_display, 'target_content_type',
+        action_object_display, 'action_object_content_type', 'timestamp')
     list_filter = ('timestamp', 'verb',)
     fieldsets = (
         (None, {
