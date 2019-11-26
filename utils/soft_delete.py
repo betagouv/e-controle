185864729637bb8c04ee72f41adbb2db7181fda9
Sylvain <sylvain@lazybird.co>
2019-11-26 12:20:24 +0100

    Change 'is_delete' to 'is_active' in admin

--- utils/soft_delete.py
+++ utils/soft_delete.py
@@ -39,18 +39,18 @@ class IsDeletedFilter(admin.SimpleListFilter):
 
 
 class SoftDeletedAdmin(object):
-
-    def is_deleted(self, instance):
-        return instance.is_deleted()
-    is_deleted.boolean = True
-    is_deleted.short_description = "deleted"
     actions = [soft_delete, undelete]
+
+    def is_active(self, instance):
+        return not instance.is_deleted()
+    is_active.boolean = True
+    is_active.short_description = "active"
 
     def get_list_filter(self, request):
         return super().get_list_filter(request) + (IsDeletedFilter,)
 
     def get_list_display(self, request):
-        return super().get_list_display(request) + ('deleted_at', 'is_deleted')
+        return super().get_list_display(request) + ('deleted_at', 'is_active')
 
     def get_readonly_fields(self, request, obj=None):
         return super().get_readonly_fields(request, obj) + ('deleted_at',)
