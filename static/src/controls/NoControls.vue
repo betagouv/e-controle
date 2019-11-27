471f0f22cc932c9e79b916e08a0bc98636ddd201
Estelle Comment <estelle.comment@gmail.com>
2019-11-27 18:00:09 +0100

    No controls page

--- static/src/controls/NoControls.vue
+++ static/src/controls/NoControls.vue
@@ -1,6 +1,40 @@
 <template>
-  <div>
-     no controooool
+  <div class="flex-column align-items-center">
+    <div class="page-title text-large mt-6 mb-2">
+      Bienvenue sur e.contrôle!
+    </div>
+    <div class="card-title mb-8">
+      Quelques étapes pour bien démarrer...
+    </div>
+    <div class="flex-row">
+      <div class="card mx-2">
+        <div class="card-status card-status-top bg-blue"></div>
+        <div class="card-body flex-row align-items-center">
+          <i class="fa fa-2x fa-archive text-muted mr-4 p-4 border rounded"></i>
+          <div>
+            1. Créez un espace de dépôt pour chaque organisme interrogé.
+          </div>
+        </div>
+      </div>
+      <div class="card mx-2">
+        <div class="card-status card-status-top bg-blue"></div>
+        <div class="card-body flex-row align-items-center">
+          <i class="fe fa-2x fe-list text-muted mr-4 p-4 border rounded"></i>
+          <div>
+            2. Créez vos questionnaires pour chaque espace de dépôt.
+          </div>
+        </div>
+      </div>
+      <div class="card mx-2">
+        <div class="card-status card-status-top bg-blue"></div>
+        <div class="card-body flex-row align-items-center">
+          <i class="fa fa-2x fa-rocket text-muted mr-4 p-4 border rounded"></i>
+          <div>
+            3. Publiez le questionnaire et visualisez les réponses dans votre Explorateur Windows.
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
