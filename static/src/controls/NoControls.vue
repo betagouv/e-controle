7208a9bb647c06a3fb2fe079337038449330f893
Estelle Comment <estelle.comment@gmail.com>
2019-11-28 15:06:46 +0100

    Update card content

--- static/src/controls/NoControls.vue
+++ static/src/controls/NoControls.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="flex-column align-items-center">
     <div class="page-title text-large mt-6 mb-2">
-      Bienvenue sur e.contrôle!
+      Bienvenue sur e.contrôle !
     </div>
     <div class="card-title mb-8">
       Quelques étapes pour bien démarrer...
     </div>
-    <div class="flex-row">
+    <div class="flex-column col-lg-11 col-xl-7">
       <div class="card mx-2">
         <div class="card-status card-status-top bg-blue"></div>
         <div class="card-body flex-row align-items-center">
           <i class="fa fa-2x fa-archive text-muted mr-4 p-4 border rounded"></i>
-          <div>
+          <div class="card-title">
             1. Créez un espace de dépôt pour chaque organisme interrogé.
           </div>
         </div>
@@ -20,7 +20,7 @@
         <div class="card-status card-status-top bg-blue"></div>
         <div class="card-body flex-row align-items-center">
           <i class="fe fa-2x fe-list text-muted mr-4 p-4 border rounded"></i>
-          <div>
+          <div class="card-title">
             2. Créez vos questionnaires pour chaque espace de dépôt.
           </div>
         </div>
@@ -28,9 +28,18 @@
       <div class="card mx-2">
         <div class="card-status card-status-top bg-blue"></div>
         <div class="card-body flex-row align-items-center">
+          <i class="fe fa-2x fe-users text-muted mr-4 p-4 border rounded"></i>
+          <div class="card-title">
+            3. Partagez l'espace de dépôt avec les membres de votre équipe et ceux de l'organisme interrogé.
+          </div>
+        </div>
+      </div>
+      <div class="card mx-2">
+        <div class="card-status card-status-top bg-blue"></div>
+        <div class="card-body flex-row align-items-center">
           <i class="fa fa-2x fa-rocket text-muted mr-4 p-4 border rounded"></i>
-          <div>
-            3. Publiez le questionnaire et visualisez les réponses dans votre Explorateur Windows.
+          <div class="card-title">
+            4. Publiez le questionnaire et visualisez les réponses dans votre Explorateur Windows.
           </div>
         </div>
       </div>
