b0270bbbd303376b10142c8d8e83bf09bbaff035
Estelle Comment <estelle.comment@beta.gouv.fr>
2019-12-06 12:17:23 +0100

    Add load status in store for fetching current user

--- static/src/store.js
+++ static/src/store.js
@@ -6,12 +6,19 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const loadStatuses = {
+  LOADING: 'LOADING',
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
+}
+
 export const store = new Vuex.Store({
   state: {
     editingControl: {},
     editingUser: {},
     editingProfileType: '',
     sessionUser: {},
+    loadStatus: loadStatuses.LOADING,
   },
   getters: {
     getField,
@@ -21,12 +28,20 @@ export const store = new Vuex.Store({
     updateSessionUser(state, user) {
       state.sessionUser = user
     },
+    updateLoadStatus(state, newStatus) {
+      state.loadStatus = newStatus
+    },
   },
   actions: {
     fetchSessionUser({ commit }) {
       axios.get(backend.currentUser()).then((response) => {
+        console.debug('Got current user', response.data)
         commit('updateSessionUser', response.data)
-      }) // todo error case
+        commit('updateLoadStatus', loadStatuses.SUCCESS)
+      }).catch(err => {
+        console.error('Error fetching current user', err)
+        commit('updateLoadStatus', loadStatuses.ERROR)
+      })
     },
   },
 })
