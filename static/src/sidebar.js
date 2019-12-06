fcd9ced463e3252180eaad096de55c956b1e704f
Estelle Comment <estelle.comment@beta.gouv.fr>
2019-12-06 10:40:26 +0100

    Lint files

--- static/src/sidebar.js
+++ static/src/sidebar.js
@@ -1,10 +1,10 @@
-import "@babel/polyfill"
+import '@babel/polyfill'
 import './utils/polyfills.js'
 
 import Sidebar from './utils/Sidebar'
-import Vue from 'vue/dist/vue.js'
+import Vue from 'vue'
 
-new Vue({
+new Vue({ // eslint-disable-line no-new
   el: '#sidebar-vm',
   components: {
     Sidebar,
--- static/src/store.js
+++ static/src/store.js
@@ -1,32 +1,31 @@
-import { getField, updateField } from 'vuex-map-fields';
 import axios from 'axios'
+import { getField, updateField } from 'vuex-map-fields'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex);
-
+Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-    state: {
-      editingControl: {},
-      editingUser: {},
-      editingProfileType: '',
-      sessionUser: {}
+  state: {
+    editingControl: {},
+    editingUser: {},
+    editingProfileType: '',
+    sessionUser: {},
+  },
+  getters: {
+    getField,
+  },
+  mutations: {
+    updateField,
+    setSessionUser(state, user) {
+      state.sessionUser = user
     },
-    getters: {
-      getField,
+  },
+  actions: {
+    setSessionUser({ commit }) {
+      axios.get('/api/user/current/').then((response) => {
+        commit('setSessionUser', response.data)
+      })
     },
-    mutations: {
-      updateField,
-      setSessionUser(state, user) {
-          state.sessionUser = user
-      }
-    },
-    actions: {
-      setSessionUser({commit}) {
-        axios.get('/api/user/current/').then((response) => {
-          commit('setSessionUser', response.data)
-        })
-      }
-    }
+  },
 })
