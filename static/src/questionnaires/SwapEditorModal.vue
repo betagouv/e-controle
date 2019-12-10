35cf39a6d7cface04246f8192386a0f96e734194
Sylvain <sylvain@lazybird.co>
2019-12-10 22:28:06 +0100

    Fix some html

--- static/src/questionnaires/SwapEditorModal.vue
+++ static/src/questionnaires/SwapEditorModal.vue
@@ -113,25 +113,25 @@
                           Libérer les droits
                       </button>
                     </div>
+                  </div>
                 </div>
               </div>
 
               <div class="card">
-                <div class="alert alert-info" role="alert">
-                  <div class="mb-4">
-                  <h4><i class="fe fe-help-circle mr-1"></i>Un problème, une question ?</h4>
-                  Nous sommes là pour vous aider. N'hésitez pas à prendre contact avec l'équipe e.contrôle !
+                  <div class="card-body alert alert-info" role="alert">
+                    <div class="mb-4">
+                    <h4><i class="fe fe-help-circle mr-1"></i>Un problème, une question ?</h4>
+                    Nous sommes là pour vous aider. N'hésitez pas à prendre contact avec l'équipe e.contrôle !
+                    </div>
+                    <button type="submit"
+                      class="btn btn-primary"
+                      title="Contacter le support e-contrôle">
+                      <i class="fe fe-mail mr-1"></i>
+                      Contacter le support e.contrôle
+                    </button>
                   </div>
-                  <button type="submit"
-                    class="btn btn-primary"
-                    title="Contacter le support e-contrôle">
-                    <i class="fe fe-mail mr-1"></i>
-                    Contacter le support e.contrôle
-                  </button>
-                </div>
               </div>
 
-            </div>
 
           </div>
         </div>
