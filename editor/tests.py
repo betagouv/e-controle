8f912b564d380a8eb472454549f7e6a1a50b2b00
Estelle Comment <estelle.comment@gmail.com>
2019-12-12 17:37:38 +0100

    Write tests.

--- editor/tests.py
+++ editor/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.shortcuts import reverse
+from pytest import mark
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from tests import factories, utils
+from user_profiles.models import UserProfile
+
+pytestmark = mark.django_db
+client = APIClient()
+
+def call_api(user, questionnaire_id, editor_id):
+    utils.login(client, user=user)
+    url = reverse('update-editor', args=[questionnaire_id])
+    post_data = {
+      'editor': editor_id
+    }
+    return client.put(url, post_data, format='json')
+
+
+def test_success():
+    control = factories.ControlFactory()
+    user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
+    questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
+
+    response = call_api(user, questionnaire.id, user.id)
+
+    assert response.status_code == 200
+
+def test_audited_cannot_access():
+    control = factories.ControlFactory()
+    user = utils.make_audited_user(control)
+    questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
+
+    response = call_api(user, questionnaire.id, user.id)
+
+    assert 400 <= response.status_code < 500
+
