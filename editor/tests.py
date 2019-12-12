f6ec3e67b648c0efb64dacd02c2dfd7d8f540cc7
Estelle Comment <estelle.comment@gmail.com>
2019-12-12 17:57:17 +0100

    Assert value of editor

--- editor/tests.py
+++ editor/tests.py
@@ -2,6 +2,7 @@ from django.shortcuts import reverse
 from pytest import mark
 from rest_framework.test import APIClient
 
+from control.models import Questionnaire
 from tests import factories, utils
 from user_profiles.models import UserProfile
 
@@ -16,67 +17,84 @@ def call_api(user, questionnaire_id, editor_id):
     }
     return client.put(url, post_data, format='json')
 
+def assert_questionnaire_has_editor(questionnaire, user):
+    assert Questionnaire.objects.get(id=questionnaire.id).editor == user
+
+
+# Success cases
+
 # the inspector resets themselves as editor on a draft questionnaire from a control they already edit.
 # (not a real use case, but it's the most basic test)
 def test_success_reset():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
+    assert_questionnaire_has_editor(questionnaire, user)
 
     response = call_api(user, questionnaire.id, user.id)
 
     assert response.status_code == 200
+    assert_questionnaire_has_editor(questionnaire, user)
 
 def test_success_force_get_rights():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     other_user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=other_user)
+    assert_questionnaire_has_editor(questionnaire, other_user)
 
     response = call_api(user, questionnaire.id, user.id)
 
     assert response.status_code == 200
+    assert_questionnaire_has_editor(questionnaire, user)
 
 def test_success_transfer_rights():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     other_user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
+    assert_questionnaire_has_editor(questionnaire, user)
 
     response = call_api(user, questionnaire.id, other_user.id)
 
     assert response.status_code == 200
+    assert_questionnaire_has_editor(questionnaire, other_user)
 
 def test_success_abandon_rights():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
+    assert_questionnaire_has_editor(questionnaire, user)
 
     response = call_api(user, questionnaire.id, None)
 
     assert response.status_code == 200
+    assert_questionnaire_has_editor(questionnaire, None)
 
 def test_success_get_rights_on_unedited_questionnaire():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=None)
+    assert_questionnaire_has_editor(questionnaire, None)
 
     response = call_api(user, questionnaire.id, user.id)
 
     assert response.status_code == 200
+    assert_questionnaire_has_editor(questionnaire, user)
 
 
-# todo check the saved questionnaire has a new editor
-
+# Fail cases
 
 def test_fail_audited_cannot_access_api():
     control = factories.ControlFactory()
     user = utils.make_audited_user(control)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
+    assert_questionnaire_has_editor(questionnaire, user)
 
     response = call_api(user, questionnaire.id, user.id)
 
     assert 400 <= response.status_code < 500
+    assert_questionnaire_has_editor(questionnaire, user)
 
 def test_fail_if_inspector_doesnt_own_questionnaire():
     control = factories.ControlFactory()
@@ -101,6 +119,7 @@ def test_fail_query_without_editor_is_refused():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
+    assert_questionnaire_has_editor(questionnaire, user)
 
     utils.login(client, user=user)
     url = reverse('update-editor', args=[questionnaire.id])
@@ -109,3 +128,4 @@ def test_fail_query_without_editor_is_refused():
     response = client.put(url, post_data, format='json')
 
     assert 400 <= response.status_code < 500
+    assert_questionnaire_has_editor(questionnaire, user)
