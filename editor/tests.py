bebdd83e105eb3232a32e15ff0bd67dc62f923f4
Estelle Comment <estelle.comment@gmail.com>
2019-12-16 11:29:25 +0100

    Rename tests

--- editor/tests.py
+++ editor/tests.py
@@ -25,7 +25,7 @@ def assert_questionnaire_has_editor(questionnaire, user):
 
 # the inspector resets themselves as editor on a draft questionnaire from a control they already edit.
 # (not a real use case, but it's the most basic test)
-def test_success_reset():
+def test_editor_can_reset_editor():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
@@ -36,7 +36,7 @@ def test_success_reset():
     assert response.status_code == 200
     assert_questionnaire_has_editor(questionnaire, user)
 
-def test_success_force_get_rights():
+def test_noneditor_can_force_get_rights():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     other_user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
@@ -48,7 +48,7 @@ def test_success_force_get_rights():
     assert response.status_code == 200
     assert_questionnaire_has_editor(questionnaire, user)
 
-def test_success_transfer_rights():
+def test_editor_can_transfer_rights():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     other_user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
@@ -60,7 +60,7 @@ def test_success_transfer_rights():
     assert response.status_code == 200
     assert_questionnaire_has_editor(questionnaire, other_user)
 
-def test_success_abandon_rights():
+def test_editor_can_abandon_rights():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
@@ -71,7 +71,7 @@ def test_success_abandon_rights():
     assert response.status_code == 200
     assert_questionnaire_has_editor(questionnaire, None)
 
-def test_success_get_rights_on_unedited_questionnaire():
+def test_noneditor_can_get_rights_on_questionnaire_without_editor():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=None)
@@ -85,7 +85,7 @@ def test_success_get_rights_on_unedited_questionnaire():
 
 # Fail cases
 
-def test_fail_audited_cannot_access_api():
+def test_audited_cannot_access_api():
     control = factories.ControlFactory()
     user = utils.make_audited_user(control)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
@@ -96,7 +96,7 @@ def test_fail_audited_cannot_access_api():
     assert 400 <= response.status_code < 500
     assert_questionnaire_has_editor(questionnaire, user)
 
-def test_fail_if_inspector_doesnt_own_questionnaire():
+def test_user_cannot_set_editor_if_they_cannot_access_the_questionnaire():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control=None, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True)
@@ -105,7 +105,7 @@ def test_fail_if_inspector_doesnt_own_questionnaire():
 
     assert 400 <= response.status_code < 500
 
-def test_fail_if_questionnaire_not_draft():
+def test_user_cannot_set_editor_if_questionnaire_is_not_draft():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=False)
@@ -115,7 +115,7 @@ def test_fail_if_questionnaire_not_draft():
     assert 400 <= response.status_code < 500
 
 # A query with no "editor" field in the JSON is a bad query, and will not unset the editor.
-def test_fail_query_without_editor_is_refused():
+def test_query_without_editor_is_refused():
     control = factories.ControlFactory()
     user = utils.make_inspector_user(control, assign_questionnaire_editor=False)
     questionnaire = factories.QuestionnaireFactory(control=control, is_draft=True, editor=user)
