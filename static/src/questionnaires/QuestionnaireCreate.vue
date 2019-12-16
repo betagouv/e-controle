4408dab9b706da76244f6b2aeb97fc27a9f9fffa
Sylvain <sylvain@lazybird.co>
2019-12-16 11:17:19 +0100

    Move swap editor button to a separate vuejs component

--- static/src/questionnaires/QuestionnaireCreate.vue
+++ static/src/questionnaires/QuestionnaireCreate.vue
@@ -1,24 +1,6 @@
 <template>
   <div>
-    <div class="alert alert-info flex-row justify-content-between" role="alert">
-        <div class="mt-2">
-        <i class="fe fe-users mr-1"></i>
-        <strong>Vous</strong> êtes actuellement le rédacteur de ce questionnaire.
-        </div>
-        <div class="text-right">
-        <button type="submit"
-          class="btn btn-primary"
-          title="Transférer les droits de rédaction..."
-          data-toggle="modal"
-          data-target="#swapEditorModal">
-          <i class="fa fa-exchange-alt mr-1"></i>
-          Transférer les droits de rédaction...
-        </button>
-        </div>
-    </div>
-
-    <swap-editor-modal id="swapEditorModal"></swap-editor-modal>
-    <swap-editor-success-modal id="swapEditorSuccessModal"></swap-editor-success-modal>
+    <swap-editor-button ></swap-editor-button>
 
     <div class="page-header">
       <div class="page-title flex-wrap">
@@ -117,8 +99,7 @@ import moment from 'moment'
 import QuestionnaireBodyCreate from './QuestionnaireBodyCreate'
 import QuestionnaireMetadataCreate from './QuestionnaireMetadataCreate'
 import QuestionnairePreview from './QuestionnairePreview'
-import SwapEditorModal from '../editors/SwapEditorModal'
-import SwapEditorSuccessModal from '../editors/SwapEditorSuccessModal'
+import SwapEditorButton from '../editors/SwapEditorButton'
 import Vue from 'vue'
 
 // State machine
@@ -153,12 +134,10 @@ export default Vue.extend({
   },
   components: {
     EmptyModal,
-    InfoBar,
     QuestionnaireBodyCreate,
     QuestionnaireMetadataCreate,
     QuestionnairePreview,
-    SwapEditorModal,
-    SwapEditorSuccessModal,
+    SwapEditorButton,
   },
   mounted() {
     console.debug('questionnaireId', this.questionnaireId)
