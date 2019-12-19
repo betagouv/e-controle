847d60e14e90fbd6711dd126762ed8fbcaece01f
Sylvain <sylvain@lazybird.co>
2019-12-19 12:11:58 +0100

    Fix some indentation

--- static/src/editors/RequestEditorButton.vue
+++ static/src/editors/RequestEditorButton.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-      <div class="alert alert-secondary flex-row justify-content-between" role="alert">
-        <div class="mt-2">
+    <div class="alert alert-secondary flex-row justify-content-between" role="alert">
+      <div class="mt-2">
         <i class="fe fe-users mr-1"></i>
         <span v-if="questionnaire.editor"><strong>{{ questionnaire.editor.first_name }} {{ questionnaire.editor.last_name }}</strong></span>
         <span v-else>[INCONNU]</span>
         est actuellement la seule personne qui peut modifier ce questionnaire
-        </div>
-        <div class="text-right">
+      </div>
+      <div class="text-right">
         <button type="submit"
           class="btn btn-gray"
           title="Demander les droits de rédaction..."
@@ -16,7 +16,7 @@
           <i class="fa fa-exchange-alt mr-1"></i>
           Demander les droits de rédaction...
         </button>
-        </div>
+      </div>
     </div>
     <request-editor-modal id="requestEditorModal" :questionnaire='questionnaire'></request-editor-modal>
     <become-editor-modal id="becomeEditorModal" :questionnaire-id='questionnaire.id'></become-editor-modal>
