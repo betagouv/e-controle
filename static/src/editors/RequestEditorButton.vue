b3ae639dda3de711db553d8078046265f2e6ac10
Estelle Comment <estelle.comment@gmail.com>
2020-01-07 17:10:49 +0100

    Add error bar

--- static/src/editors/RequestEditorButton.vue
+++ static/src/editors/RequestEditorButton.vue
@@ -1,36 +1,41 @@
 <template>
   <div>
-    <div class="alert alert-secondary flex-row justify-content-between" role="alert">
-      <div class="mt-2">
-        <i class="fe fe-users mr-1"></i>
-        <span v-if="questionnaire.editor">
-          <strong>{{ questionnaire.editor.first_name }} {{ questionnaire.editor.last_name }}</strong>
-          est actuellement la seule personne qui peut modifier ce questionnaire
-        </span>
-        <span v-else>
-          Personne n'est actuellement affecté à la rédaction de ce questionnaire.
-        </span>
+    <div class="alert alert-secondary" role="alert">
+      <div class="flex-row justify-content-between align-items-center">
+        <div>
+          <i class="fe fe-users mr-1"></i>
+          <span v-if="questionnaire.editor">
+            <strong>{{ questionnaire.editor.first_name }} {{ questionnaire.editor.last_name }}</strong>
+            est actuellement la seule personne qui peut modifier ce questionnaire
+          </span>
+          <span v-else>
+            Personne n'est actuellement affecté à la rédaction de ce questionnaire.
+          </span>
+        </div>
+        <div class="text-right">
+          <button v-if="questionnaire.editor"
+            type="submit"
+            class="btn btn-gray"
+            title="Obtenir les droits de rédaction..."
+            data-toggle="modal"
+            data-target="#requestEditorModal">
+            <i class="fa fa-exchange-alt mr-1"></i>
+            <span>Obtenir les droits de rédaction...</span>
+          </button>
+          <button v-else
+            type="submit"
+            class="btn btn-gray"
+            title="Obtenir les droits de rédaction..."
+            @click="switchToEditorPage"
+            >
+            <i class="fa fa-exchange-alt mr-1"></i>
+            <span>Obtenir les droits de rédaction...</span>
+          </button>
+        </div>
       </div>
-      <div class="text-right">
-        <button v-if="questionnaire.editor"
-          type="submit"
-          class="btn btn-gray"
-          title="Obtenir les droits de rédaction..."
-          data-toggle="modal"
-          data-target="#requestEditorModal">
-          <i class="fa fa-exchange-alt mr-1"></i>
-          <span>Obtenir les droits de rédaction...</span>
-        </button>
-        <button v-else
-          type="submit"
-          class="btn btn-gray"
-          title="Obtenir les droits de rédaction..."
-          @click="switchToEditorPage"
-          >
-          <i class="fa fa-exchange-alt mr-1"></i>
-          <span>Obtenir les droits de rédaction...</span>
-        </button>
-      </div>
+      <error-bar class="mt-4">
+        bli
+      </error-bar>
     </div>
     <request-editor-modal id="requestEditorModal" :questionnaire='questionnaire'></request-editor-modal>
     <become-editor-modal id="becomeEditorModal" :questionnaire-id='questionnaire.id'></become-editor-modal>
@@ -40,6 +45,7 @@
 <script>
 import backendUrls from '../utils/backend.js'
 import BecomeEditorModal from '../editors/BecomeEditorModal'
+import ErrorBar from '../utils/ErrorBar'
 import { mapFields } from 'vuex-map-fields'
 import RequestEditorModal from '../editors/RequestEditorModal'
 import Vue from 'vue'
@@ -47,8 +53,9 @@ import Vue from 'vue'
 export default Vue.extend({
   props: ['questionnaire'],
   components: {
-    RequestEditorModal,
     BecomeEditorModal,
+    ErrorBar,
+    RequestEditorModal,
   },
   computed: {
     ...mapFields(['sessionUser']),
