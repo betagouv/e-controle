ed40c697c43142c4ef90d56fd0e5eda46b394b18
Estelle Comment <estelle.comment@gmail.com>
2020-01-10 11:32:19 +0100

    Refactor SwapEditorSuccessModal.vue for easier reuse

--- static/src/editors/SwapEditorSuccessModal.vue
+++ static/src/editors/SwapEditorSuccessModal.vue
@@ -1,21 +1,19 @@
 <template>
   <empty-modal no-close="true">
-    <div class="modal-header border-bottom-0 flex-column align-items-center">
-      <p>
-        <i class="fe fe-check-circle fg-success big-icon"></i>
-      </p>
-      <h4 class="text-center">
-        Les droits de rédaction ont été transférés !
-      </h4>
+    <div class="modal-header pb-0 border-bottom-0 flex-column align-items-center">
+      <i class="fe fe-check-circle fg-success big-icon"></i>
     </div>
     <div class="modal-body text-center">
+      <h4 class="mb-6">
+        Les droits de rédaction ont été transférés !
+      </h4>
       <p>
         Pour devenir rédacteur de ce questionnaire à nouveau, il faudra que
         votre collègue transfère ou libère les droits de rédaction.
       </p>
     </div>
     <div class="modal-footer border-top-0 d-flex justify-content-center">
-      <button @click="goToReadonlyPage()" role="button" class="btn btn-primary">
+      <button @click="goToReadonlyPage()" role="button" class="btn btn-primary px-7">
         OK
       </button>
     </div>
