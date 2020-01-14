2d1a6fdad6d92915709f131a8b038e3638958def
Estelle Comment <estelle.comment@beta.gouv.fr>
2020-01-14 10:43:03 +0100

    Fix QuestionnaireBodyCreate tests

--- static/src/questionnaires/test/QuestionnaireBodyCreate.spec.js
+++ static/src/questionnaires/test/QuestionnaireBodyCreate.spec.js
@@ -1,9 +1,30 @@
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
+import { getField, updateField } from 'vuex-map-fields'
 import QuestionnaireBodyCreate from '../QuestionnaireBodyCreate.vue'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('QuestionnaireBodyCreate.vue', () => {
+  let store
+  const currentQuestionnaire = { id: 12345 }
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state: {
+        currentQuestionnaire: currentQuestionnaire,
+      },
+      getters: {
+        getField,
+      },
+      mutations: {
+        updateField,
+      },
+    })
+  })
+
   test('is a Vue instance', () => {
-    const wrapper = mount(QuestionnaireBodyCreate)
+    const wrapper = mount(QuestionnaireBodyCreate, { store, localVue })
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
