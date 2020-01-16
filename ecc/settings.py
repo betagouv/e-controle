c5f41c1b1e8924777c3d5cf718f42b9ffe262616
Sylvain <sylvain@lazybird.co>
2020-01-17 00:52:59 +0100

    Add sent URL in settings

--- ecc/settings.py
+++ ecc/settings.py
@@ -191,6 +191,7 @@ MAGICAUTH_EMAIL_SUBJECT = 'Connexion e.controle'
 MAGICAUTH_EMAIL_HTML_TEMPLATE = 'login/email.html'
 MAGICAUTH_EMAIL_TEXT_TEMPLATE = 'login/email.txt'
 MAGICAUTH_LOGIN_VIEW_TEMPLATE = 'login/login.html'
+MAGICAUTH_EMAIL_SENT_VIEW_TEMPLATE = 'login/email_sent.html'
 
 LOGIN_URL = 'login'
 
