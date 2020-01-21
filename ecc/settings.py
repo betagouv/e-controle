6bbac507e9f50c03fc1c84f9cfd3e33bf392e157
Sylvain <sylvain@lazybird.co>
2020-01-21 17:49:07 +0100

    Change magicauth login URL path

--- ecc/settings.py
+++ ecc/settings.py
@@ -192,6 +192,7 @@ MAGICAUTH_EMAIL_HTML_TEMPLATE = 'login/email.html'
 MAGICAUTH_EMAIL_TEXT_TEMPLATE = 'login/email.txt'
 MAGICAUTH_LOGIN_VIEW_TEMPLATE = 'login/login.html'
 MAGICAUTH_EMAIL_SENT_VIEW_TEMPLATE = 'login/email_sent.html'
+MAGICAUTH_LOGIN_URL = ''
 
 LOGIN_URL = 'login'
 
