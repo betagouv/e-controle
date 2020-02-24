c6765749bc917528d98e70f5564afc0de7d606ba
Estelle Comment <estelle.comment@gmail.com>
2020-02-24 16:15:27 +0100

    Better move_file script

--- scripts/move_file.py
+++ scripts/move_file.py
@@ -1,18 +1,16 @@
 import os
 
-response_file_id = 670
-# move from question 3 to 2
-initial_question = 'Q01-T01-03'
-new_question = 'Q01-T01-02'
+def move_file(response_file_id, new_order):
+  response_file = ResponseFile.objects.get(id=response_file_id)
+  initial_path = response_file.file.path
+  initial_name = response_file.file.name
+  initial_prefix = f'Q{response_file.question.theme.questionnaire.numbering:02}-T{response_file.question.theme.numbering:02}-{response_file.question.numbering:02}'
+  new_prefix = f'Q{response_file.question.theme.questionnaire.numbering:02}-T{response_file.question.theme.numbering:02}-{(new_order + 1):02}'
 
-response_file = ResponseFile.objects.get(id=response_file_id)
-initial_path = response_file.file.path
-initial_name = response_file.file.name
+  new_name = initial_name.replace(initial_prefix, new_prefix, 1)
+  new_path = settings.MEDIA_ROOT + '/' + new_name
 
-new_name = initial_name.replace(initial_question, new_question, 1)
-new_path = settings.MEDIA_ROOT + '/' + new_name
+  response_file.file.name = new_name
+  os.rename(initial_path, new_path) # needs sudo rights
 
-response_file.file.name = new_name
-os.rename(initial_path, new_path) # needs sudo rights
-
-response_file.save()
+  response_file.save()
