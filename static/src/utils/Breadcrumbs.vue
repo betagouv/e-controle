5cb4c9de388f3f1af71dc39b3f08981e7598a70c
Estelle Comment <estelle.comment@beta.gouv.fr>
2020-03-31 16:43:36 +0200

    Add link in breadcrumb

--- static/src/utils/Breadcrumbs.vue
+++ static/src/utils/Breadcrumbs.vue
@@ -7,19 +7,27 @@
       <i class="fa fa-chevron-right"></i>
     </div>
     <div class="mx-2">
-      {{ control.reference_code }} -
-      {{ control.depositing_organization !== undefined ?
-            control.depositing_organization : control.title}}
+      <a :href="controlLink">
+        {{ control.reference_code }} -
+        {{ control.depositing_organization !== undefined ?
+              control.depositing_organization : control.title}}
+      </a>
     </div>
   </h5>
 </template>
 
 <script>
+import backendUrls from './backend'
 import Vue from 'vue'
 
 export default Vue.extend({
   props: {
     control: Object,
+  },
+  computed: {
+    controlLink() {
+      return backendUrls['control-detail'](this.control.id)
+    },
   },
 })
 </script>
