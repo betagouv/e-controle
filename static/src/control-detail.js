5241ae0d4cf48eca55d647f2369fe7e7777d779a
Estelle Comment <estelle.comment@beta.gouv.fr>
2020-04-15 15:53:31 +0200

    Extra comments

--- static/src/control-detail.js
+++ static/src/control-detail.js
@@ -8,10 +8,16 @@ import ControlPage from './controls/ControlPage'
 
 Vue.use(Vuex)
 
-// XSS-safe way to get JSON data from server : write it to html (django template does html encoding)
-// and then fetch it into JS using safe DOM manipulation functions.
-// Source :
-// https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#html-entity-encoding
+/*
+XSS-safe way to get JSON data from server : write it to html (django template does html encoding)
+and then fetch it into JS using safe DOM manipulation functions.
+Source :
+https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#html-entity-encoding
+
+A simpler safe way to get unsafe server data into Vue would be to get it through an AJAX request,
+instead of passing it through server templates.
+It does adds a delay for the user, since they will wait for the ajax-requested data.
+*/
 const controlsDataEl = document.getElementById('controls-data')
 const userDataEl = document.getElementById('user-data')
 // decode and parse the content of the div
