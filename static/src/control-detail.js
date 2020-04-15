2ae208e09e9378b9db48fded8dcb3058c83d5dfe
Estelle Comment <estelle.comment@beta.gouv.fr>
2020-04-15 15:23:56 +0200

    Pass JSON data from server in xss-safe way, in control_detail.html

--- static/src/control-detail.js
+++ static/src/control-detail.js
@@ -8,12 +8,27 @@ import ControlPage from './controls/ControlPage'
 
 Vue.use(Vuex)
 
+// XSS-safe way to get JSON data from server : write it to html (django template does html encoding)
+// and then fetch it into JS using safe DOM manipulation functions.
+// Source :
+// https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#html-entity-encoding
+const controlsDataEl = document.getElementById('controls-data')
+const userDataEl = document.getElementById('user-data')
+// decode and parse the content of the div
+const controls = JSON.parse(controlsDataEl.textContent)
+const user = JSON.parse(userDataEl.textContent)
+
 new Vue({ // eslint-disable-line no-new
   store,
   el: '#control-detail-vm',
-  components: {
+  render: h => h(
     ControlPage,
-  },
+    {
+      props: {
+        controls: controls,
+        user: user,
+      },
+    }),
   methods: {
     ...mapActions(['fetchConfig', 'fetchSessionUser']),
 
