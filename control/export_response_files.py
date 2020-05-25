7521d4ccf0a409ec3a2ba0fe99152e5a82f67a39
Estelle Comment <estelle.comment@beta.gouv.fr>
2020-05-25 18:32:23 +0200

    Fill in real data (formatted dates are still fake)

--- control/export_response_files.py
+++ control/export_response_files.py
@@ -14,14 +14,18 @@ def generate_response_file_list_in_csv(questionnaire):
           'Horodatage de dépôt, format triable par ordre chronologique',
           'Personne qui a déposé',
         ])
-        csvwriter.writerow([
-          '1',
-          'Comptabilité',
-          '1.2',
-          'fichier1.pdf',
-          'Mer 13 mai 2020 à 16:09:33',
-          '2020-05-13T16:09:33.595213+02:00',
-          'Julie Delacour'
-        ])
+        for theme in questionnaire.themes.all():
+            for question in theme.questions.all():
+                for file in question.response_files.all():
+                    csvwriter.writerow([
+                      theme.numbering,
+                      theme.title,
+                      f'{theme.numbering}.{question.numbering}',
+                      file.basename,
+                      'Mer 13 mai 2020 à 16:09:33',  # todo real formatted date
+                      file.created,
+                      f'{file.author.first_name} {file.author.last_name}'
+                    ])
+
         return f
     # todo delete temp file when we are done
