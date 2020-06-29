1fd4c8dc452ea1d2f631d319c43e3312b177fd51
Sylvain <sylvain@lazybird.co>
2020-06-29 14:39:11 +0200

    Remove unused runner class

--- control/tests/test_send_response_file_xls.py
+++ control/tests/test_send_response_file_xls.py
@@ -8,15 +8,6 @@ from tests import factories, utils
 pytestmark = mark.django_db
 
 
-class SendResponseFileXLSRunner:
-    def __init__(self, client):
-        questionnaire = factories.QuestionnaireFactory(is_draft=False)
-        user = utils.make_audited_user(questionnaire.control)
-        utils.login(client, user=user)
-        url = reverse('send-response-file-list', args=[questionnaire.id])
-        self.response = client.get(url)
-
-
 def test_download_response_file_xls_works_if_the_control_is_associated_with_the_user(client):
     questionnaire = factories.QuestionnaireFactory(is_draft=False)
     user = utils.make_audited_user(questionnaire.control)
