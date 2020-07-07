8d128079ff618f1e925becba86ec8bf7bf5605f3
Estelle Comment <estelle.comment@beta.gouv.fr>
2020-07-07 11:13:54 +0200

    Rename test file to match url

--- control/tests/test_send_response_file_list.py
+++ control/tests/test_send_response_file_list.py
@@ -8,7 +8,7 @@ from tests import factories, utils
 pytestmark = mark.django_db
 
 
-def test_download_response_file_xls_works_if_the_control_is_associated_with_the_user(client):
+def test_download_response_file_list_works_if_the_control_is_associated_with_the_user(client):
     questionnaire = factories.QuestionnaireFactory(is_draft=False)
     user = utils.make_audited_user(questionnaire.control)
     utils.login(client, user=user)
@@ -17,7 +17,7 @@ def test_download_response_file_xls_works_if_the_control_is_associated_with_the_
     assert response.status_code == 200
 
 
-def test_download_response_file_xls_fails_if_the_control_is_not_associated_with_the_user(client):
+def test_download_response_file_list_fails_if_the_control_is_not_associated_with_the_user(client):
     questionnaire = factories.QuestionnaireFactory(is_draft=False)
     unauthorized_control = factories.ControlFactory()
     user = utils.make_audited_user(unauthorized_control)
