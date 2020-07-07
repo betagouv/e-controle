7074720c2cbe296887ff49b5d3b4e9b55b4d8c99
Estelle Comment <estelle.comment@beta.gouv.fr>
2020-07-07 11:16:51 +0200

    Extra tests for inspectors

--- control/tests/test_send_response_file_list.py
+++ control/tests/test_send_response_file_list.py
@@ -8,7 +8,7 @@ from tests import factories, utils
 pytestmark = mark.django_db
 
 
-def test_download_response_file_list_works_if_the_control_is_associated_with_the_user(client):
+def test_download_response_file_list_works_for_audited_if_the_control_is_associated_with_the_user(client):
     questionnaire = factories.QuestionnaireFactory(is_draft=False)
     user = utils.make_audited_user(questionnaire.control)
     utils.login(client, user=user)
@@ -17,10 +17,28 @@ def test_download_response_file_list_works_if_the_control_is_associated_with_the
     assert response.status_code == 200
 
 
-def test_download_response_file_list_fails_if_the_control_is_not_associated_with_the_user(client):
+def test_download_response_file_list_fails_for_audited_if_the_control_is_not_associated_with_the_user(client):
     questionnaire = factories.QuestionnaireFactory(is_draft=False)
     unauthorized_control = factories.ControlFactory()
     user = utils.make_audited_user(unauthorized_control)
+    utils.login(client, user=user)
+    url = reverse('send-response-file-list', args=[questionnaire.id])
+    response = client.get(url)
+    assert response.status_code != 200
+
+def test_download_response_file_list_works_for_inspector_if_the_control_is_associated_with_the_user(client):
+    questionnaire = factories.QuestionnaireFactory(is_draft=False)
+    user = utils.make_inspector_user(questionnaire.control)
+    utils.login(client, user=user)
+    url = reverse('send-response-file-list', args=[questionnaire.id])
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+def test_download_response_file_list_fails_for_inspector_if_the_control_is_not_associated_with_the_user(client):
+    questionnaire = factories.QuestionnaireFactory(is_draft=False)
+    unauthorized_control = factories.ControlFactory()
+    user = utils.make_inspector_user(unauthorized_control)
     utils.login(client, user=user)
     url = reverse('send-response-file-list', args=[questionnaire.id])
     response = client.get(url)
