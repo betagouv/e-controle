9ef15c96060958d49285b2261d1924f874a2a053
Sylvain <sylvain@lazybird.co>
2020-07-15 23:42:02 +0200

    Add test for backoffice admin login

---
+++ backoffice/tests.py
@@ -0,0 +1,46 @@
+from pytest import mark
+
+from django.test import override_settings
+from django.contrib.auth import get_user_model
+from django.shortcuts import reverse
+
+
+pytestmark = mark.django_db
+
+User = get_user_model()
+
+
+ADMIN_USER_DATA = {
+  'username': 'test@test.com',
+  'email': 'test@test.com',
+  'password': 'test12345',
+}
+
+
+def test_super_user_can_login_to_admin(client):
+    User.objects.create_superuser(**ADMIN_USER_DATA)
+    assert '_auth_user_id' not in client.session
+    url = reverse('admin-login')
+    client.post(url, ADMIN_USER_DATA)
+    assert '_auth_user_id' in client.session
+
+
+@override_settings(ADMIN_URL='admin/')
+def test_login_to_admin_redirects(client):
+    User.objects.create_superuser(**ADMIN_USER_DATA)
+    url = reverse('admin-login')
+    response = client.post(url, ADMIN_USER_DATA)
+    assert response.status_code == 302
+    assert 'admin/' in response.url
+
+
+@override_settings(ADMIN_URL='admin/')
+def test_login_to_admin_does_not_redirect_to_next(client):
+    User.objects.create_superuser(**ADMIN_USER_DATA)
+    url = reverse('admin-login')
+    url += '?next=/unwanted/'
+    print(url)
+    response = client.post(url, ADMIN_USER_DATA)
+    assert response.status_code == 302
+    assert '/unwanted/' not in response.url
+    assert 'admin/' in response.url
