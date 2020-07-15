f8de7174e2ebc89e54083026c37d27cf8aa66948
Sylvain <sylvain@lazybird.co>
2020-07-15 17:39:36 +0200

    Update doc on backoffice app

--- backoffice/views.py
+++ backoffice/views.py
@@ -8,10 +8,7 @@ class AdminLoginView(LoginView):
     We don't use the default admin login view because it uses
     "?next=" redirections which are not safe.
     """
-    template_name = 'admin/login.html'
+    template_name = 'admin/login.html'  # Let's use the template shipped with Django
 
     def get_success_url(self):
-        """
-        We override that default behaviour beacause we don't want next URL redirection.
-        """
         return resolve_url('admin:index')
