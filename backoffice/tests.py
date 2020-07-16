092ea4b681da6c04b71fb60fb662a738819ff582
Sylvain <sylvain@lazybird.co>
2020-07-16 10:47:11 +0200

    Updated admin login password

--- backoffice/tests.py
+++ backoffice/tests.py
@@ -25,6 +25,16 @@ def test_super_user_can_login_to_admin(client):
     assert '_auth_user_id' in client.session
 
 
+def test_login_to_admin_fails_if_wrong_credentials(client):
+    User.objects.create_superuser(**ADMIN_USER_DATA)
+    wrong_data = ADMIN_USER_DATA.copy()
+    wrong_data['password'] = 'wrong_password'
+    assert '_auth_user_id' not in client.session
+    url = reverse('admin-login')
+    client.post(url, wrong_data)
+    assert '_auth_user_id' not in client.session
+
+
 @override_settings(ADMIN_URL='admin/')
 def test_login_to_admin_redirects(client):
     User.objects.create_superuser(**ADMIN_USER_DATA)
@@ -39,7 +49,6 @@ def test_login_to_admin_does_not_redirect_to_next(client):
     User.objects.create_superuser(**ADMIN_USER_DATA)
     url = reverse('admin-login')
     url += '?next=/unwanted/'
-    print(url)
     response = client.post(url, ADMIN_USER_DATA)
     assert response.status_code == 302
     assert '/unwanted/' not in response.url
