516bb91745816917f4dbc28a73cacea8b711b2c5
Ali Tiane <alitoch@gmail.com>
2021-10-08 16:15:49 +0200

    Updating package version and adding warning msg

--- static/src/questionnaires/PublishFlow.vue
+++ static/src/questionnaires/PublishFlow.vue
@@ -1,6 +1,17 @@
 <template>
   <modal-flow ref="modalFlow" :action-function="publishFunction">
     <template v-slot:confirm-modal-form>
+      <div
+        class="alert alert-warning alert-icon"
+        v-if="!getAuditedUsers().length"
+      >
+        <i class="fa fa-exclamation-circle mr-2" aria-hidden="true"></i>
+        <div class="mb-4">
+          <strong>Attention !</strong>
+          Vous n'avez pas sélectionné d'organisme de contrôle. Êtes-vous sûr de
+          vouloir continuer ?
+        </div>
+      </div>
       <div class="modal-header border-bottom-0">
         <h4 class="modal-title">
           Vous y êtes presque! En cochant ces mentions, vous êtes informés que :
@@ -9,25 +20,19 @@
 
       <div class="modal-body">
         <label class="custom-control custom-checkbox">
-          <input type="checkbox"
-                  class="custom-control-input"
-                  required>
+          <input type="checkbox" class="custom-control-input" required />
           <span class="custom-control-label">
             Le questionnaire ne pourra plus être modifié
           </span>
         </label>
         <label class="custom-control custom-checkbox">
-          <input type="checkbox"
-                  class="custom-control-input"
-                  required>
+          <input type="checkbox" class="custom-control-input" required />
           <span class="custom-control-label">
             Le questionnaire deviendra visible par l'organisme interrogé
           </span>
         </label>
         <label class="custom-control custom-checkbox">
-          <input type="checkbox"
-                  class="custom-control-input"
-                  required>
+          <input type="checkbox" class="custom-control-input" required />
           <span class="custom-control-label">
             Vous devrez informer l'organisme interrogé
           </span>
@@ -35,17 +40,19 @@
       </div>
 
       <div class="modal-footer border-top-0">
-        <button type="button"
-                class="btn btn-secondary"
-                data-dismiss="modal"
-                title="J'ai encore des modifications à faire"
+        <button
+          type="button"
+          class="btn btn-secondary"
+          data-dismiss="modal"
+          title="J'ai encore des modifications à faire"
         >
           <i class="fa fa-chevron-left mr-2"></i>
           J'ai encore des modifications à faire
         </button>
-        <button type="submit"
-                class="btn btn-primary"
-                title="Publier le questionnaire"
+        <button
+          type="submit"
+          class="btn btn-primary"
+          title="Publier le questionnaire"
         >
           <i class="fa fa-rocket mr-1"></i>
           Publier le questionnaire
@@ -58,17 +65,19 @@
     </template>
 
     <template v-slot:error-message>
-      <div>
-        Le questionnaire n'a pas pu être publié. Vous pouvez réessayer.
-      </div>
+      <div>Le questionnaire n'a pas pu être publié. Vous pouvez réessayer.</div>
       <div>
         Si l'erreur persiste, vous pouvez contacter
-        <a :href="'mailto:' + config.support_team_email +
-                  '?subject=Erreur lors de la publication : ' +
-                  $refs.modalFlow.error.message"
-            class="text-nowrap"
-            target="_blank"
-            rel="noopener noreferrer"
+        <a
+          :href="
+            'mailto:' +
+            config.support_team_email +
+            '?subject=Erreur lors de la publication : ' +
+            $refs.modalFlow.error.message
+          "
+          class="text-nowrap"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           {{ config.support_team_email }}
         </a>
@@ -84,48 +93,50 @@
         <p>
           <i class="fe fe-check-circle fg-success big-icon"></i>
         </p>
-        <h4 class="text-center">
-          Bravo, votre questionnaire est publié!
-        </h4>
+        <h4 class="text-center">Bravo, votre questionnaire est publié!</h4>
         <div class="mt-5">
-            <p>Pensez à informer l'organisme contrôlé.</p>
-            <p>Si des réponses sont déposées par l'organisme interrogé, vous recevrez un email de
-          notification dès le lendemain 8 heures.</p>
+          <p>Pensez à informer l'organisme contrôlé.</p>
+          <p>
+            Si des réponses sont déposées par l'organisme interrogé, vous
+            recevrez un email de notification dès le lendemain 8 heures.
+          </p>
         </div>
       </div>
       <div class="modal-body text-center">
         <div class="mt-5 flex-row justify-content-center">
-          <button type="button"
-            class="btn btn-primary ml-2"
-            @click="goHome"
-          >
+          <button type="button" class="btn btn-primary ml-2" @click="goHome">
             <i class="fa fa-chevron-left mr-2"></i>
             Revenir à l'accueil
           </button>
-          <a class="btn btn-primary ml-2"
-              :href="'mailto:' + emailHeader.audited +
-                    '?cc=' + emailHeader.editors +
-                    '&subject=Questionnaire publié' +
-                    '&body=' + emailBody"
-              target="_blank"
-              rel="noopener noreferrer"
+          <a
+            class="btn btn-primary ml-2"
+            :href="
+              'mailto:' +
+              emailHeader.audited +
+              '?cc=' +
+              emailHeader.editors +
+              '&subject=Questionnaire publié' +
+              '&body=' +
+              emailBody
+            "
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Créer un mail pour l'informer
           </a>
         </div>
       </div>
     </template>
-
   </modal-flow>
 </template>
 
 <script>
-import axios from 'axios'
-import backend from '../utils/backend'
-import { mapFields } from 'vuex-map-fields'
-import { mapState } from 'vuex'
-import ModalFlow from '../utils/ModalFlow'
-import Vue from 'vue'
+import axios from "axios";
+import backend from "../utils/backend";
+import { mapFields } from "vuex-map-fields";
+import { mapState } from "vuex";
+import ModalFlow from "../utils/ModalFlow";
+import Vue from "vue";
 
 export default Vue.extend({
   components: {
@@ -134,7 +145,7 @@ export default Vue.extend({
   data() {
     return {
       users: [],
-    }
+    };
   },
   props: {
     questionnaire: Object,
@@ -146,57 +157,73 @@ export default Vue.extend({
     },
   },
   computed: {
-    ...mapFields([
-      'config',
-    ]),
+    ...mapFields(["config"]),
     ...mapState({
-      controls: 'controls',
+      controls: "controls",
     }),
-    emailHeader: function() {
-      const uniq = (arrArg) => arrArg.filter((elem, pos, arr) => arr.indexOf(elem) === pos)
-      const currentControl = this.controls.find(control => control.id === this.questionnaire.control)
+    emailHeader: function () {
+      const uniq = (arrArg) =>
+        arrArg.filter((elem, pos, arr) => arr.indexOf(elem) === pos);
+      const currentControl = this.controls.find(
+        (control) => control.id === this.questionnaire.control
+      );
 
       if (currentControl) {
-        const editors = uniq(currentControl.questionnaires.map(q => q.editor.email)).join(',')
-        const audited = this.users.filter(u => u.profile_type === 'audited').map(u => u.email).join(',')
-        return { editors, audited }
+        const editors = uniq(
+          currentControl.questionnaires.map((q) => q.editor.email)
+        ).join(";");
+        const audited = this.users
+          .filter((u) => u.profile_type === "audited")
+          .map((u) => u.email)
+          .join(";");
+        return { editors, audited };
       }
 
-      return {}
+      return {};
     },
-    emailBody: function() {
-      const newline = '%0d%0a'
-      const currentControl = this.controls.find(control => control.id === this.questionnaire.control)
-      const expiryDateString = this.questionnaire.end_date === null ? '' : `${newline}${newline}La date limite de réponse est le ${this.questionnaire.end_date}.`
+    emailBody: function () {
+      const newline = "%0d%0a";
+      const currentControl = this.controls.find(
+        (control) => control.id === this.questionnaire.control
+      );
+      const expiryDateString =
+        this.questionnaire.end_date === null
+          ? ""
+          : `${newline}${newline}La date limite de réponse est le ${this.questionnaire.end_date}.`;
 
       if (currentControl) {
-        return `Bonjour,${newline}${newline}Un nouveau questionnaire vient d'être ajouté au contrôle ${currentControl.title}. Il s'agit du questionnaire numéro ${this.questionnaire.id} : ${this.questionnaire.title}.${expiryDateString}${newline}${newline}Nous vous invitons à vous connecter à e-contrôle pour le voir et apporter vos réponses, au lien ci-dessous :${newline}${newline}https://e-controle-beta.ccomptes.fr${newline}${newline}Cordialement,`
+        return `Bonjour,${newline}${newline}Un nouveau questionnaire vient d'être ajouté au contrôle ${currentControl.title}. Il s'agit du questionnaire numéro ${this.questionnaire.id} : ${this.questionnaire.title}.${expiryDateString}${newline}${newline}Nous vous invitons à vous connecter à e-contrôle pour le voir et apporter vos réponses, au lien ci-dessous :${newline}${newline}https://e-controle-beta.ccomptes.fr${newline}${newline}Cordialement,`;
       }
 
-      return ''
+      return "";
     },
   },
   methods: {
     getUsers() {
-      axios.get(backend.getUsersInControl(this.controlId))
-        .then((response) => {
-          this.users = response.data
-        })
+      axios.get(backend.getUsersInControl(this.controlId)).then((response) => {
+        this.users = response.data;
+      });
+    },
+    getAuditedUsers() {
+      return this.users.filter((item) => {
+        return item.profile_type === "audited";
+      });
     },
     start() {
-      console.debug('outer start!')
-      this.$refs.modalFlow.start()
+      console.debug("outer start!");
+      this.$refs.modalFlow.start();
     },
     goHome() {
-      this.window.location.href = backend['control-detail'](this.questionnaire.control)
+      this.window.location.href = backend["control-detail"](
+        this.questionnaire.control
+      );
     },
   },
   mounted() {
-    this.getUsers()
+    this.getUsers();
   },
-})
+});
 </script>
 
 <style scoped>
-
 </style>
